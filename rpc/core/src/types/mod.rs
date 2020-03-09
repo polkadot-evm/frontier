28af3abdfe96efb00130f6ee565c2324ac7f0c9d
Wei Tang <hi@that.world>
2020-03-09 03:46:37 +0100

    Remove all unused types

--- rpc/core/src/types/mod.rs
+++ rpc/core/src/types/mod.rs
@@ -21,23 +21,11 @@ mod block;
 mod block_number;
 mod bytes;
 mod call_request;
-// mod confirmations;
-// mod consensus_status;
-// mod derivation;
 mod filter;
-// mod histogram;
 mod index;
 mod log;
-// mod node_kind;
-// mod private_receipt;
-// mod private_log;
-// mod provenance;
 mod receipt;
-// mod rpc_settings;
-// mod secretstore;
 mod sync;
-// mod trace;
-// mod trace_filter;
 mod transaction;
 mod transaction_request;
 mod transaction_condition;
@@ -50,34 +38,15 @@ pub use self::bytes::Bytes;
 pub use self::block::{RichBlock, Block, BlockTransactions, Header, RichHeader, Rich};
 pub use self::block_number::BlockNumber;
 pub use self::call_request::CallRequest;
-// pub use self::confirmations::{
-// 	ConfirmationPayload, ConfirmationRequest, ConfirmationResponse, ConfirmationResponseWithToken,
-// 	TransactionModification, EIP191SignRequest, EthSignRequest, DecryptRequest, Either
-// };
-// pub use self::consensus_status::*;
-// pub use self::derivation::{DeriveHash, DeriveHierarchical, Derive};
 pub use self::filter::{Filter, FilterChanges};
-// pub use self::histogram::Histogram;
 pub use self::index::Index;
 pub use self::log::Log;
-// pub use self::node_kind::{NodeKind, Availability, Capability};
-// pub use self::private_receipt::{PrivateTransactionReceipt, PrivateTransactionReceiptAndTransaction};
-// pub use self::private_log::PrivateTransactionLog;
-// pub use self::provenance::Origin;
 pub use self::receipt::Receipt;
-// pub use self::rpc_settings::RpcSettings;
-// pub use self::secretstore::EncryptedDocumentKey;
 pub use self::sync::{
 	SyncStatus, SyncInfo, Peers, PeerInfo, PeerNetworkInfo, PeerProtocolsInfo,
 	TransactionStats, ChainStatus, EthProtocolInfo, PipProtocolInfo,
 };
-// pub use self::trace::{LocalizedTrace, TraceResults, TraceResultsWithTransactionHash};
-// pub use self::trace_filter::TraceFilter;
 pub use self::transaction::{Transaction, RichRawTransaction, LocalTransactionStatus};
 pub use self::transaction_request::TransactionRequest;
 pub use self::transaction_condition::TransactionCondition;
 pub use self::work::Work;
-
-// // TODO [ToDr] Refactor to a proper type Vec of enums?
-// /// Expected tracing type.
-// pub type TraceOptions = Vec<String>;
