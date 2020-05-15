c8732d45c619976e04dcd01fd61cf0de6007f842
Wei Tang <hi@that.world>
2020-05-15 19:48:18 +0200

    Block and receipt storage

--- frame/ethereum/src/lib.rs
+++ frame/ethereum/src/lib.rs
@@ -101,36 +101,7 @@ decl_storage! {
 	// storage items are isolated from other pallets.
 	// ---------------------------------vvvvvvv
 	trait Store for Module<T: Trait> as Example {
-		// Any storage declarations of the form:
-		//   `pub? Name get(fn getter_name)? [config()|config(myname)] [build(|_| {...})] : <type> (= <new_default_value>)?;`
-		// where `<type>` is either:
-		//   - `Type` (a basic value item); or
-		//   - `map hasher(HasherKind) KeyType => ValueType` (a map item).
-		//
-		// Note that there are two optional modifiers for the storage type declaration.
-		// - `Foo: Option<u32>`:
-		//   - `Foo::put(1); Foo::get()` returns `Some(1)`;
-		//   - `Foo::kill(); Foo::get()` returns `None`.
-		// - `Foo: u32`:
-		//   - `Foo::put(1); Foo::get()` returns `1`;
-		//   - `Foo::kill(); Foo::get()` returns `0` (u32::default()).
-		// e.g. Foo: u32;
-		// e.g. pub Bar get(fn bar): map hasher(blake2_128_concat) T::AccountId => Vec<(T::Balance, u64)>;
-		//
-		// For basic value items, you'll get a type which implements
-		// `frame_support::StorageValue`. For map items, you'll get a type which
-		// implements `frame_support::StorageMap`.
-		//
-		// If they have a getter (`get(getter_name)`), then your pallet will come
-		// equipped with `fn getter_name() -> Type` for basic value items or
-		// `fn getter_name(key: KeyType) -> ValueType` for map items.
-		Dummy get(fn dummy) config(): Option<T::Balance>;
-
-		// A map that has enumerable entries.
-		Bar get(fn bar) config(): map hasher(blake2_128_concat) T::AccountId => T::Balance;
-
-		// this one uses the default, we'll demonstrate the usage of 'mutate' API.
-		Foo get(fn foo) config(): T::Balance;
+		BlocksAndReceipts: map hasher(blake2_128_concat) T::Hash => Option<(ethereum::Block, Vec<ethereum::Receipt>)>;
 	}
 }
 
@@ -187,100 +158,6 @@ decl_module! {
 		/// For non-generic events, the generic parameter just needs to be dropped, so that it
 		/// looks like: `fn deposit_event() = default;`.
 		fn deposit_event() = default;
-		/// This is your public interface. Be extremely careful.
-		/// This is just a simple example of how to interact with the pallet from the external
-		/// world.
-		// This just increases the value of `Dummy` by `increase_by`.
-		//
-		// Since this is a dispatched function there are two extremely important things to
-		// remember:
-		//
-		// - MUST NOT PANIC: Under no circumstances (save, perhaps, storage getting into an
-		// irreparably damaged state) must this function panic.
-		// - NO SIDE-EFFECTS ON ERROR: This function must either complete totally (and return
-		// `Ok(())` or it must have no side-effects on storage and return `Err('Some reason')`.
-		//
-		// The first is relatively easy to audit for - just ensure all panickers are removed from
-		// logic that executes in production (which you do anyway, right?!). To ensure the second
-		// is followed, you should do all tests for validity at the top of your function. This
-		// is stuff like checking the sender (`origin`) or that state is such that the operation
-		// makes sense.
-		//
-		// Once you've determined that it's all good, then enact the operation and change storage.
-		// If you can't be certain that the operation will succeed without substantial computation
-		// then you have a classic blockchain attack scenario. The normal way of managing this is
-		// to attach a bond to the operation. As the first major alteration of storage, reserve
-		// some value from the sender's account (`Balances` Pallet has a `reserve` function for
-		// exactly this scenario). This amount should be enough to cover any costs of the
-		// substantial execution in case it turns out that you can't proceed with the operation.
-		//
-		// If it eventually transpires that the operation is fine and, therefore, that the
-		// expense of the checks should be borne by the network, then you can refund the reserved
-		// deposit. If, however, the operation turns out to be invalid and the computation is
-		// wasted, then you can burn it or repatriate elsewhere.
-		//
-		// Security bonds ensure that attackers can't game it by ensuring that anyone interacting
-		// with the system either progresses it or pays for the trouble of faffing around with
-		// no progress.
-		//
-		// If you don't respect these rules, it is likely that your chain will be attackable.
-		//
-		// Each transaction can define an optional `#[weight]` attribute to convey a set of static
-		// information about its dispatch. FRAME System and FRAME Executive pallet then use this
-		// information to properly execute the transaction, whilst keeping the total load of the
-		// chain in a moderate rate.
-		//
-		// The _right-hand-side_ value of the `#[weight]` attribute can be any type that implements
-		// a set of traits, namely [`WeighData`] and [`ClassifyDispatch`]. The former conveys the
-		// weight (a numeric representation of pure execution time and difficulty) of the
-		// transaction and the latter demonstrates the [`DispatchClass`] of the call. A higher
-		// weight means a larger transaction (less of which can be placed in a single block).
-		#[weight = 0]
-		fn accumulate_dummy(origin, increase_by: T::Balance) -> DispatchResult {
-			// This is a public call, so we ensure that the origin is some signed account.
-			let _sender = ensure_signed(origin)?;
-
-			// Read the value of dummy from storage.
-			// let dummy = Self::dummy();
-			// Will also work using the `::get` on the storage item type itself:
-			// let dummy = <Dummy<T>>::get();
-
-			// Calculate the new value.
-			// let new_dummy = dummy.map_or(increase_by, |dummy| dummy + increase_by);
-
-			// Put the new value into storage.
-			// <Dummy<T>>::put(new_dummy);
-			// Will also work with a reference:
-			// <Dummy<T>>::put(&new_dummy);
-
-			// Here's the new one of read and then modify the value.
-			<Dummy<T>>::mutate(|dummy| {
-				let new_dummy = dummy.map_or(increase_by, |dummy| dummy + increase_by);
-				*dummy = Some(new_dummy);
-			});
-
-			// Let's deposit an event to let the outside world know this happened.
-			Self::deposit_event(RawEvent::Dummy(increase_by));
-
-			// All good.
-			Ok(())
-		}
-
-		/// A privileged call; in this case it resets our dummy value to something new.
-		// Implementation of a privileged call. The `origin` parameter is ROOT because
-		// it's not (directly) from an extrinsic, but rather the system as a whole has decided
-		// to execute it. Different runtimes have different reasons for allow privileged
-		// calls to be executed - we don't need to care why. Because it's privileged, we can
-		// assume it's a one-off operation and substantial processing/storage/memory can be used
-		// without worrying about gameability or attack scenarios.
-		// If you do not specify `Result` explicitly as return value, it will be added automatically
-		// for you and `Ok(())` will be returned.
-		#[weight = WeightForSetDummy::<T>(<BalanceOf<T>>::from(100u32))]
-		fn set_dummy(origin, #[compact] new_value: T::Balance) {
-			ensure_root(origin)?;
-			// Put the new value into storage.
-			<Dummy<T>>::put(new_value);
-		}
 
 		// The signature could also look like: `fn on_initialize()`.
 		// This function could also very well have a weight annotation, similar to any other. The
@@ -294,9 +171,7 @@ decl_module! {
 
 		// The signature could also look like: `fn on_finalize()`
 		fn on_finalize(_n: T::BlockNumber) {
-			// Anything that needs to be done at the end of the block.
-			// We just kill our dummy storage item.
-			<Dummy<T>>::kill();
+
 		}
 
 		// A runtime code run after every block and have access to extended set of APIs.
@@ -316,21 +191,7 @@ decl_module! {
 // functions that do not write to storage and operation functions that do.
 // - Private functions. These are your usual private utilities unavailable to other pallets.
 impl<T: Trait> Module<T> {
-	// Add public immutables and private mutables.
-	#[allow(dead_code)]
-	fn accumulate_foo(origin: T::Origin, increase_by: T::Balance) -> DispatchResult {
-		let _sender = ensure_signed(origin)?;
 
-		let prev = <Foo<T>>::get();
-		// Because Foo has 'default', the type of 'foo' in closure is the raw type instead of an Option<> type.
-		let result = <Foo<T>>::mutate(|foo| {
-			*foo = *foo + increase_by;
-			*foo
-		});
-		assert!(prev + increase_by == result);
-
-		Ok(())
-	}
 }
 
 // Similar to other FRAME pallets, your pallet can also define a signed extension and perform some
@@ -404,13 +265,6 @@ impl<T: Trait + Send + Sync> SignedExtension for WatchDummy<T> {
 
 		// check for `set_dummy`
 		match call {
-			Call::set_dummy(..) => {
-				sp_runtime::print("set_dummy was received.");
-
-				let mut valid_tx = ValidTransaction::default();
-				valid_tx.priority = Bounded::max_value();
-				Ok(valid_tx)
-			}
 			_ => Ok(Default::default()),
 		}
 	}
