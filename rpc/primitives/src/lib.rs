f57f06f22b8011f5d8cac656d0a03b43934c5358
Wei Tang <hi@that.world>
2020-05-25 10:45:12 +0200

    Add client ref to EthApi implementation

--- rpc/primitives/src/lib.rs
+++ rpc/primitives/src/lib.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
+#![cfg_attr(not(feature = "std"), no_std)]
+
 sp_api::decl_runtime_apis! {
 	/// API necessary for Ethereum-compatibility layer.
 	pub trait EthereumRuntimeApi {
