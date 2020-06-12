e7a79729e801831b19c3bcf30c3e86b1a5b27612
Crystalin <alan.sapede@gmail.com>
2020-06-12 00:14:56 -0400

    Adds better test settup for ethereum frame

--- frame/ethereum/src/mock.rs
+++ frame/ethereum/src/mock.rs
@@ -18,15 +18,16 @@
 
 use super::*;
 use crate::{Module, Trait};
+use ethereum::{TransactionAction, TransactionSignature};
 use frame_support::{impl_outer_origin, parameter_types, weights::Weight};
 use pallet_evm::{FeeCalculator, HashTruncateConvertAccountId};
+use rlp::*;
 use sp_core::{H160, H256, U256};
 use sp_runtime::{
     testing::Header,
     traits::{BlakeTwo256, IdentityLookup},
     ModuleId, Perbill,
 };
-use std::str::FromStr;
 
 impl_outer_origin! {
     pub enum Origin for Test where system = frame_system {}
@@ -124,45 +125,36 @@ pub type Evm = pallet_evm::Module<Test>;
 pub struct AccountInfo {
     pub address: H160,
     pub private_key: H256,
-    pub r: H256,
-    pub s: H256,
-    pub v: u64,
 }
 
 // This function basically just builds a genesis storage key/value store according to
 // our desired mockup.
-pub fn new_test_ext() -> (Vec<AccountInfo>, sp_io::TestExternalities) {
-    let storage = frame_system::GenesisConfig::default()
+pub fn new_test_ext(accounts_len: usize) -> (Vec<AccountInfo>, sp_io::TestExternalities) {
+    let ext = frame_system::GenesisConfig::default()
         .build_storage::<Test>()
-        .unwrap();
-    let ext = storage.into();
+        .unwrap()
+        .into();
 
-    // TODO: Replace with a correct way to generate those.
-    const ALICE_ACCOUNT: &str = "3725421C2ED3bB336a6Ebd4A06D2f3721679e47f";
-    const ALICE_PRIVATE_KEY: &str =
-        "f12a96d6ee380d41d87ba35e3d26dc1d8c8718b37578baae94c0460a5b26d3e3";
-    const ALICE_V: u64 = 0x78;
-    const ALICE_R: &str = "e5b8c39851a99396f1844171c99dd66a24f44df3c07c02925e3f547e653abc22";
-    const ALICE_S: &str = "01570aba1ab55d32103e275f6b0ddda5d18a55b927f056e1eeb5238a77d004c1";
-
-    let alice_account = H160::from_str(ALICE_ACCOUNT).unwrap();
-    let alice_private_key = H256::from_str(ALICE_PRIVATE_KEY).unwrap();
-    let alice_r = H256::from_str(ALICE_R).unwrap();
-    let alice_s = H256::from_str(ALICE_S).unwrap();
-
-    let pairs = vec![AccountInfo {
-        address: alice_account,
-        private_key: alice_private_key,
-        r: alice_r,
-        s: alice_s,
-        v: ALICE_V,
-    }];
+    let pairs = (0..accounts_len)
+        .map(|i| {
+            let private_key = H256::from_slice(&[(i + 1) as u8; 32]); //H256::from_low_u64_be((i + 1) as u64);
+            let secret_key = secp256k1::SecretKey::parse_slice(&private_key[..]).unwrap();
+            let public_key = secp256k1::PublicKey::from_secret_key(&secret_key);
+            let address = H160::from(H256::from_slice(
+                &Keccak256::digest(&public_key.serialize()[1..])[..],
+            ));
+            AccountInfo {
+                private_key: private_key,
+                address: address,
+            }
+        })
+        .collect::<Vec<_>>();
 
     (pairs, ext)
 }
 
 pub fn contract_address(sender: H160, nonce: u64) -> H160 {
-    let mut rlp = rlp::RlpStream::new_list(2);
+    let mut rlp = RlpStream::new_list(2);
     rlp.append(&sender);
     rlp.append(&nonce);
 
@@ -173,4 +165,64 @@ pub fn storage_address(sender: H160, slot: H256) -> H256 {
     H256::from_slice(&Keccak256::digest(
         [&H256::from(sender)[..], &slot[..]].concat().as_slice(),
     ))
+}
+
+pub struct UnsignedTransaction {
+    pub nonce: U256,
+    pub gas_price: U256,
+    pub gas_limit: U256,
+    pub action: TransactionAction,
+    pub value: U256,
+    pub input: Vec<u8>,
+}
+
+impl UnsignedTransaction {
+    fn signing_rlp_append(&self, s: &mut RlpStream) {
+        s.begin_list(9);
+        s.append(&self.nonce);
+        s.append(&self.gas_price);
+        s.append(&self.gas_limit);
+        s.append(&self.action);
+        s.append(&self.value);
+        s.append(&self.input);
+        s.append(&42u8); // TODO: move this chain id into the frame ethereum configuration
+        s.append(&0u8);
+        s.append(&0u8);
+    }
+
+    fn signing_hash(&self) -> H256 {
+        let mut stream = RlpStream::new();
+        self.signing_rlp_append(&mut stream);
+        H256::from_slice(&Keccak256::digest(&stream.drain()).as_slice())
+    }
+
+    pub fn sign(self, key: &H256) -> Transaction {
+        let hash = self.signing_hash();
+        let msg = {
+            let mut a = [0u8; 32];
+            for i in 0..32 {
+                a[i] = hash[i];
+            }
+            secp256k1::Message::parse(&a)
+        };
+        let s = secp256k1::sign(&msg, &secp256k1::SecretKey::parse_slice(&key[..]).unwrap());
+        let sig = s.0.serialize();
+
+        let sig = TransactionSignature::new(
+            0x78,
+            H256::from_slice(&sig[0..32]),
+            H256::from_slice(&sig[32..64]),
+        )
+        .unwrap();
+
+        Transaction {
+            nonce: self.nonce,
+            gas_price: self.gas_price,
+            gas_limit: self.gas_limit,
+            action: self.action,
+            value: self.value,
+            input: self.input,
+            signature: sig,
+        }
+    }
 }
