29b1edd6c51bc95bf5c7f62e9c5763004f7e50c7
tgmichel <telmo@purestake.com>
2020-06-16 11:14:41 +0200

    Update substrate pin 45e9f09 (#41)
    * Update substrate pin 45e9f09
    
    * Renaming ServiceBuilder.build

--- template/node/src/service.rs
+++ template/node/src/service.rs
@@ -143,7 +143,7 @@ pub fn new_full(config: Configuration) -> Result<impl AbstractService, ServiceEr
 			let provider = client as Arc<dyn StorageAndProofProvider<_, _>>;
 			Ok(Arc::new(GrandpaFinalityProofProvider::new(backend, provider)) as _)
 		})?
-		.build()?;
+		.build_full()?;
 
 	if role.is_authority() {
 		let proposer = sc_basic_authorship::ProposerFactory::new(
@@ -305,5 +305,5 @@ pub fn new_light(config: Configuration) -> Result<impl AbstractService, ServiceE
 
 			Ok(crate::rpc::create_light(light_deps))
 		})?
-		.build()
+		.build_light()
 }
