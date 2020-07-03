92f660f3073cd6a63a4e51db4a0b66a163b31548
tgmichel <telmo@purestake.com>
2020-07-03 11:35:41 +0200

    Support r field in transaction_build

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -151,7 +151,7 @@ fn transaction_build(
 		},
 		standard_v: U256::from(transaction.signature.standard_v()),
 		v: U256::from(transaction.signature.v()),
-		r: U256::zero(), // TODO
+		r: U256::from(transaction.signature.r().as_bytes()),
 		s: U256::zero(), // TODO
 		condition: None // TODO
 	}
