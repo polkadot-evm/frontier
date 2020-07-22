09a69e20211faf9ae369464cb8fdf59628e81490
tgmichel <telmo@purestake.com>
2020-07-22 16:31:47 +0200

    Document EthereumRuntimeApi and README update (#81)
    * Document EthereumRuntimeApi
    
    * Update readme
    
    * Styling fixes

--- rpc/primitives/src/lib.rs
+++ rpc/primitives/src/lib.rs
@@ -53,12 +53,19 @@ impl Default for TransactionStatus {
 sp_api::decl_runtime_apis! {
 	/// API necessary for Ethereum-compatibility layer.
 	pub trait EthereumRuntimeApi {
+		/// Returns runtime defined pallet_evm::ChainId.
 		fn chain_id() -> u64;
+		/// Returns pallet_evm::Accounts by address.
 		fn account_basic(address: H160) -> pallet_evm::Account;
+		/// Returns FixedGasPrice::min_gas_price
 		fn gas_price() -> U256;
+		/// For a given account address, returns pallet_evm::AccountCodes.
 		fn account_code_at(address: H160) -> Vec<u8>;
+		/// Returns the converted FindAuthor::find_author authority id.
 		fn author() -> H160;
+		/// For a given account address and index, returns pallet_evm::AccountStorages.
 		fn storage_at(address: H160, index: U256) -> H256;
+		/// Returns a pallet_evm::execute_call response.
 		fn call(
 			from: H160,
 			to: H160,
@@ -68,17 +75,32 @@ sp_api::decl_runtime_apis! {
 			gas_price: U256,
 			nonce: Option<U256>,
 		) -> Option<(Vec<u8>, U256)>;
+		/// For a given block number, returns an ethereum::Block and all its TransactionStatus.
 		fn block_by_number(number: u32) -> (Option<EthereumBlock>, Vec<Option<TransactionStatus>>);
+		/// For a given block number, returns the number of transactions.
 		fn block_transaction_count_by_number(number: u32) -> Option<U256>;
+		/// For a given block hash, returns an ethereum::Block.
 		fn block_by_hash(hash: H256) -> Option<EthereumBlock>;
+		/// For a given block hash, returns an ethereum::Block and all its TransactionStatus.
 		fn block_by_hash_with_statuses(hash: H256) -> (Option<EthereumBlock>, Vec<Option<TransactionStatus>>);
+		/// For a given block hash, returns the number of transactions in a given block hash.
 		fn block_transaction_count_by_hash(hash: H256) -> Option<U256>;
+		/// For a given transaction hash, returns data necessary to build an Transaction rpc type response.
+		/// - EthereumTransaction: transaction as stored in pallet-ethereum.
+		/// - EthereumBlock: block as stored in pallet-ethereum .
+		/// - TransactionStatus: transaction execution metadata.
+		/// - EthereumReceipt: transaction receipt.
 		fn transaction_by_hash(hash: H256) -> Option<(
 			EthereumTransaction,
 			EthereumBlock,
 			TransactionStatus,
 			Vec<EthereumReceipt>
 		)>;
+		/// For a given block hash and transaction index, returns data necessary to build an Transaction rpc
+		/// type response.
+		/// - EthereumTransaction: transaction as stored in pallet-ethereum.
+		/// - EthereumBlock: block as stored in pallet-ethereum .
+		/// - TransactionStatus: transaction execution metadata.
 		fn transaction_by_block_hash_and_index(
 			hash: H256,
 			index: u32
@@ -87,6 +109,11 @@ sp_api::decl_runtime_apis! {
 			EthereumBlock,
 			TransactionStatus
 		)>;
+		/// For a given block number and transaction index, returns data necessary to build an Transaction rpc
+		/// type response.
+		/// - EthereumTransaction: transaction as stored in pallet-ethereum.
+		/// - EthereumBlock: block as stored in pallet-ethereum .
+		/// - TransactionStatus: transaction execution metadata.
 		fn transaction_by_block_number_and_index(
 			number: u32,
 			index: u32
