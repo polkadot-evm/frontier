cf3bc312046ae1735d4a348e656de0f8f0c5b711
Wei Tang <wei@that.world>
2020-07-27 16:37:29 +0200

    Clean up some pallet-ethereum comments (#88)
    * Clean up some pallet-ethereum comments
    
    * Fix template compiles

--- ts-tests/frontier-test-node/runtime/src/lib.rs
+++ ts-tests/frontier-test-node/runtime/src/lib.rs
@@ -312,7 +312,7 @@ construct_runtime!(
 		Balances: balances::{Module, Call, Storage, Config<T>, Event<T>},
 		TransactionPayment: transaction_payment::{Module, Storage},
 		Sudo: sudo::{Module, Call, Config<T>, Storage, Event<T>},
-		Ethereum: ethereum::{Module, Call, Storage, Event<T>, ValidateUnsigned},
+		Ethereum: ethereum::{Module, Call, Storage, Event, ValidateUnsigned},
 		EVM: evm::{Module, Config, Call, Storage, Event<T>},
 	}
 );
