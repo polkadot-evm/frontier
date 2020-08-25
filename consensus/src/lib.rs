e4da13fda97bf604f02a6191f66f0d71791eefae
Wei Tang <wei@that.world>
2020-08-25 06:46:24 +0200

    Simple block import wrapper

--- consensus/src/lib.rs
+++ consensus/src/lib.rs
@@ -15,3 +15,68 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use std::sync::Arc;
+use std::collections::HashMap;
+use std::marker::PhantomData;
+use sc_client_api::{BlockOf, backend::AuxStore};
+use sp_blockchain::{HeaderBackend, ProvideCache, well_known_cache_keys::Id as CacheKeyId};
+use sp_block_builder::BlockBuilder as BlockBuilderApi;
+use sp_runtime::traits::Block as BlockT;
+use sp_api::ProvideRuntimeApi;
+use sp_consensus::{
+	BlockImportParams, Error as ConsensusError, BlockImport,
+	BlockCheckParams, ImportResult,
+};
+use sc_client_api;
+
+pub struct FrontierBlockImport<B: BlockT, I, C> {
+	inner: I,
+	client: Arc<C>,
+	_marker: PhantomData<B>,
+}
+
+impl<B, I, C> FrontierBlockImport<B, I, C> where
+	B: BlockT,
+	I: BlockImport<B, Transaction = sp_api::TransactionFor<C, B>> + Send + Sync,
+	I::Error: Into<ConsensusError>,
+	C: ProvideRuntimeApi<B> + Send + Sync + HeaderBackend<B> + AuxStore + ProvideCache<B> + BlockOf,
+	C::Api: BlockBuilderApi<B, Error = sp_blockchain::Error>,
+{
+	pub fn new(
+		inner: I,
+		client: Arc<C>,
+	) -> Self {
+		Self {
+			inner,
+			client,
+			_marker: PhantomData,
+		}
+	}
+}
+
+impl<B, I, C> BlockImport<B> for FrontierBlockImport<B, I, C> where
+	B: BlockT,
+	I: BlockImport<B, Transaction = sp_api::TransactionFor<C, B>> + Send + Sync,
+	I::Error: Into<ConsensusError>,
+	C: ProvideRuntimeApi<B> + Send + Sync + HeaderBackend<B> + AuxStore + ProvideCache<B> + BlockOf,
+	C::Api: BlockBuilderApi<B, Error = sp_blockchain::Error>,
+{
+	type Error = ConsensusError;
+	type Transaction = sp_api::TransactionFor<C, B>;
+
+	fn check_block(
+		&mut self,
+		block: BlockCheckParams<B>,
+	) -> Result<ImportResult, Self::Error> {
+		self.inner.check_block(block).map_err(Into::into)
+	}
+
+	fn import_block(
+		&mut self,
+		block: BlockImportParams<B, Self::Transaction>,
+		new_cache: HashMap<CacheKeyId, Vec<u8>>,
+	) -> Result<ImportResult, Self::Error> {
+		self.inner.import_block(block, new_cache).map_err(Into::into)
+	}
+}
