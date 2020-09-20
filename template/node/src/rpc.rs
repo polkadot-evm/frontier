0cbe438c440384b0e4864e156c6a4046ca752fef
Wei Tang <wei@that.world>
2020-09-20 21:58:21 +0200

    Replace vendor ethereum with published ethereum crate and fix warnings (#138)
    * Remove ethereum vendor
    
    * Update ethereum dependency and fix most warnings

--- template/node/src/rpc.rs
+++ template/node/src/rpc.rs
@@ -21,7 +21,7 @@ use std::{sync::Arc, fmt};
 
 use sc_consensus_manual_seal::rpc::{ManualSeal, ManualSealApi};
 use frontier_template_runtime::{Hash, AccountId, Index, opaque::Block, Balance};
-use sp_api::{ProvideRuntimeApi, Core};
+use sp_api::ProvideRuntimeApi;
 use sp_transaction_pool::TransactionPool;
 use sp_blockchain::{Error as BlockChainError, HeaderMetadata, HeaderBackend};
 use sp_consensus::SelectChain;
