843f80c93363bcdc6421033d51cab707033f0fc6
tgmichel <telmo@purestake.com>
2020-09-21 19:13:01 +0200

    Add missing const

--- frame/ethereum/src/mock.rs
+++ frame/ethereum/src/mock.rs
@@ -76,6 +76,9 @@ impl frame_system::Trait for Test {
 }
 
 parameter_types! {
+	// For weight estimation, we assume that the most locks on an individual account will be 50.
+	// This number may need to be adjusted in the future if this assumption no longer holds true.
+	pub const MaxLocks: u32 = 50;
 	pub const ExistentialDeposit: u64 = 500;
 }
 
