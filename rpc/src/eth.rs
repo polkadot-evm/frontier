3e5d489b956b00cddad2fe103156f73e0de03915
Wei Tang <wei@that.world>
2020-11-16 06:50:02 +0100

    Custom signer support

--- rpc/src/eth.rs
+++ rpc/src/eth.rs
@@ -16,7 +16,10 @@
 
 use std::{marker::PhantomData, sync::Arc};
 use std::collections::BTreeMap;
-use ethereum::{Block as EthereumBlock, Transaction as EthereumTransaction};
+use ethereum::{
+	Block as EthereumBlock, Transaction as EthereumTransaction,
+	TransactionMessage as EthereumTransactionMessage,
+};
 use ethereum_types::{H160, H256, H64, U256, U64, H512};
 use jsonrpc_core::{BoxFuture, Result, futures::future::{self, Future}};
 use futures::future::TryFutureExt;
@@ -33,10 +36,11 @@ use sc_network::{NetworkService, ExHashT};
 use frontier_rpc_core::{EthApi as EthApiT, NetApi as NetApiT};
 use frontier_rpc_core::types::{
 	BlockNumber, Bytes, CallRequest, Filter, FilteredParams, Index, Log, Receipt, RichBlock,
-	SyncStatus, SyncInfo, Transaction, Work, Rich, Block, BlockTransactions, VariadicValue
+	SyncStatus, SyncInfo, Transaction, Work, Rich, Block, BlockTransactions, VariadicValue,
+	TransactionRequest,
 };
 use frontier_rpc_primitives::{EthereumRuntimeRPCApi, ConvertTransaction, TransactionStatus};
-use crate::{internal_err, error_on_execution_failure};
+use crate::{internal_err, error_on_execution_failure, EthSigner};
 
 pub use frontier_rpc_core::{EthApiServer, NetApiServer};
 use codec::{self, Encode};
@@ -47,6 +51,7 @@ pub struct EthApi<B: BlockT, C, P, CT, BE, H: ExHashT> {
 	convert_transaction: CT,
 	network: Arc<NetworkService<B, H>>,
 	is_authority: bool,
+	signers: Vec<Box<dyn EthSigner>>,
 	_marker: PhantomData<(B, BE)>,
 }
 
@@ -58,7 +63,15 @@ impl<B: BlockT, C, P, CT, BE, H: ExHashT> EthApi<B, C, P, CT, BE, H> {
 		network: Arc<NetworkService<B, H>>,
 		is_authority: bool
 	) -> Self {
-		Self { client, pool, convert_transaction, network, is_authority, _marker: PhantomData }
+		Self {
+			client,
+			pool,
+			convert_transaction,
+			network,
+			is_authority,
+			signers: Vec::new(),
+			_marker: PhantomData,
+		}
 	}
 }
 
@@ -132,9 +145,7 @@ fn transaction_build(
 	sig[0..32].copy_from_slice(&transaction.signature.r()[..]);
 	sig[32..64].copy_from_slice(&transaction.signature.s()[..]);
 	sig[64] = transaction.signature.standard_v();
-	msg.copy_from_slice(&transaction.message_hash(
-		transaction.signature.chain_id().map(u64::from)
-	)[..]);
+	msg.copy_from_slice(&EthereumTransactionMessage::from(transaction.clone()).hash()[..]);
 
 	let pubkey = match sp_io::crypto::secp256k1_ecdsa_recover(&sig, &msg) {
 		Ok(p) => Some(H512::from(p)),
@@ -169,7 +180,6 @@ fn transaction_build(
 		v: U256::from(transaction.signature.v()),
 		r: U256::from(transaction.signature.r().as_bytes()),
 		s: U256::from(transaction.signature.s().as_bytes()),
-		condition: None // TODO
 	}
 }
 
@@ -304,7 +314,11 @@ impl<B, C, P, CT, BE, H: ExHashT> EthApiT for EthApi<B, C, P, CT, BE, H> where
 	}
 
 	fn accounts(&self) -> Result<Vec<H160>> {
-		Ok(vec![])
+		let mut accounts = Vec::new();
+		for signer in &self.signers {
+			accounts.append(&mut signer.accounts());
+		}
+		Ok(accounts)
 	}
 
 	fn block_number(&self) -> Result<U256> {
@@ -484,6 +498,83 @@ impl<B, C, P, CT, BE, H: ExHashT> EthApiT for EthApi<B, C, P, CT, BE, H> where
 			);
 		}
 		Ok(Bytes(vec![]))
+	}
+
+	fn send_transaction(&self, request: TransactionRequest) -> BoxFuture<H256> {
+		let from = match request.from {
+			Some(from) => from,
+			None => {
+				let accounts = match self.accounts() {
+					Ok(accounts) => accounts,
+					Err(e) => return Box::new(future::result(Err(e))),
+				};
+
+				match accounts.get(0) {
+					Some(account) => account.clone(),
+					None => return Box::new(future::result(Err(internal_err("no signer available")))),
+				}
+			},
+		};
+
+		let nonce = match request.nonce {
+			Some(nonce) => nonce,
+			None => {
+				match self.transaction_count(from, None) {
+					Ok(nonce) => nonce,
+					Err(e) => return Box::new(future::result(Err(e))),
+				}
+			},
+		};
+
+		let chain_id = match self.chain_id() {
+			Ok(chain_id) => chain_id,
+			Err(e) => return Box::new(future::result(Err(e))),
+		};
+
+		let message = ethereum::TransactionMessage {
+			nonce,
+			gas_price: request.gas_price.unwrap_or(U256::from(1)),
+			gas_limit: request.gas.unwrap_or(U256::max_value()),
+			value: request.value.unwrap_or(U256::zero()),
+			input: request.data.map(|s| s.into_vec()).unwrap_or_default(),
+			action: match request.to {
+				Some(to) => ethereum::TransactionAction::Call(to),
+				None => ethereum::TransactionAction::Create,
+			},
+			chain_id: chain_id.map(|s| s.as_u64()),
+		};
+
+		let mut transaction = None;
+
+		for signer in &self.signers {
+			if signer.accounts().contains(&from) {
+				match signer.sign(message) {
+					Ok(t) => transaction = Some(t),
+					Err(e) => return Box::new(future::result(Err(e))),
+				}
+				break
+			}
+		}
+
+		let transaction = match transaction {
+			Some(transaction) => transaction,
+			None => return Box::new(future::result(Err(internal_err("no signer available")))),
+		};
+		let transaction_hash = H256::from_slice(
+			Keccak256::digest(&rlp::encode(&transaction)).as_slice()
+		);
+		let hash = self.client.info().best_hash;
+		Box::new(
+			self.pool
+				.submit_one(
+					&BlockId::hash(hash),
+					TransactionSource::Local,
+					self.convert_transaction.convert_transaction(transaction),
+				)
+				.compat()
+				.map(move |_| transaction_hash)
+				.map_err(|err| internal_err(format!("submit transaction to pool failed: {:?}", err)))
+		)
 	}
 
 	fn send_raw_transaction(&self, bytes: Bytes) -> BoxFuture<H256> {
