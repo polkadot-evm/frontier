a95e84802ed1113f6413bc31a8974adfcb40efef
tgmichel <telmo@purestake.com>
2020-11-20 13:25:27 +0100

    Fix checker

--- ts-tests/tests/test-web3api.ts
+++ ts-tests/tests/test-web3api.ts
@@ -5,14 +5,14 @@ import { describeWithFrontier, customRequest } from "./util";
 describeWithFrontier("Frontier RPC (Web3Api)", `simple-specs.json`, (context) => {
 
 	step("should get client version", async function () {
-        const version = await context.web3.eth.getNodeInfo();
-        expect(version).to.be.equal("node-frontier-template/v1.1/frontier-rpc-0.1.0");
-    });
-    
+		const version = await context.web3.eth.getNodeInfo();
+		expect(version).to.be.equal("node-frontier-template/v1.1/frontier-rpc-0.1.0");
+	});
+	
 	step("should remote sha3", async function () {
-        const data = context.web3.utils.stringToHex("hello");
-        const hash = await customRequest(context.web3, "web3_sha3", [data]);
-        const local_hash = context.web3.utils.sha3("hello");
-        expect(hash.result).to.be.equal(local_hash);
+		const data = context.web3.utils.stringToHex("hello");
+		const hash = await customRequest(context.web3, "web3_sha3", [data]);
+		const local_hash = context.web3.utils.sha3("hello");
+		expect(hash.result).to.be.equal(local_hash);
 	});
 });
