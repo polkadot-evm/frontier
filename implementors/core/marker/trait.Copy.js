acf7033161fd561f1c715ef56a069dae76ada0ea
sorpaas <sorpaas@users.noreply.github.com>
2020-11-24 17:50:17 +0000

    deploy: 8b7359732a01b23b20aa7b3f1ceaf7cdcece81b0

--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["fc_rpc"] = [{"text":"impl Copy for HexEncodedIdProvider","synthetic":false,"types":[]}];
 implementors["frontier_template_node"] = [{"text":"impl Copy for Sealing","synthetic":false,"types":[]}];
 implementors["frontier_template_runtime"] = [{"text":"impl Copy for Runtime","synthetic":false,"types":[]}];
 implementors["pallet_ethereum"] = [{"text":"impl&lt;T:&nbsp;Copy + Trait&gt; Copy for Module&lt;T&gt;","synthetic":false,"types":[]}];
