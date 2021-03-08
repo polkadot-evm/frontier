5beddb51c3f11fac32355eadd62d971828ce0a99
sorpaas <sorpaas@users.noreply.github.com>
2021-03-08 07:11:24 +0000

    deploy: 4cfe0193ddc02b99601b2680fc8f60899337e037

---
+++ rustdocs/fc_mapping_sync/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["sync_block",""],["sync_blocks",""],["sync_genesis_block",""],["sync_one_level",""]],"struct":[["MappingSyncWorker",""]]});
---
+++ rustdocs/implementors/futures_core/stream/trait.Stream.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["fc_mapping_sync"] = [{"text":"impl&lt;Block:&nbsp;BlockT, C, B&gt; Stream for MappingSyncWorker&lt;Block, C, B&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;C: ProvideRuntimeApi&lt;Block&gt; + Send + Sync + HeaderBackend&lt;Block&gt; + BlockOf,<br>&nbsp;&nbsp;&nbsp;&nbsp;C::Api: EthereumRuntimeRPCApi&lt;Block&gt;,<br>&nbsp;&nbsp;&nbsp;&nbsp;B: Backend&lt;Block&gt;,&nbsp;</span>","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
