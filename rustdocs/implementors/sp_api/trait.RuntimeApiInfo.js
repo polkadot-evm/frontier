fac558a6aaaea4ac9aea08df10280e338f118c11
sorpaas <sorpaas@users.noreply.github.com>
2021-03-12 11:01:44 +0000

    deploy: ed534fa1dc90085507ef112c2a9a13f54fbe1fc5

--- rustdocs/implementors/sp_api/trait.RuntimeApiInfo.js
+++ rustdocs/implementors/sp_api/trait.RuntimeApiInfo.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["fp_rpc"] = [{"text":"impl&lt;Block:&nbsp;BlockT, __Sr_Api_Error__&gt; RuntimeApiInfo for dyn EthereumRuntimeRPCApi&lt;Block, Error = __Sr_Api_Error__&gt;","synthetic":false,"types":[]}];
+implementors["fp_rpc"] = [{"text":"impl&lt;Block:&nbsp;BlockT&gt; RuntimeApiInfo for dyn EthereumRuntimeRPCApi&lt;Block&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
