9ef6fdc75585957ecafb0a6f821ad8a817456a0e
Drew Stone <drewstone329@gmail.com>
2021-03-16 10:57:12 +0200

    Sha3FIPS standard precompiles and ECDSA public key recovery (#304)
    * Sha3 and ECDSA precompiles
    
    * Tests for sha3fips
    
    * Updates to spec and adds tests
    
    * Spacing and formatting
    
    * Fmt
    
    * Comment out test
    
    * Updates
    
    * Updates
    
    * Update ECRecoverTests.sol
    
    * Remove merge conflict
    
    * Remove unnecessary yarn file
    
    * Uncomment tests
    
    * Fix tests
    
    * Reset simple-specs.json change
    
    * Remove unused frontier-spec.json file
    
    Co-authored-by: Wei Tang <accounts@that.world>
    Co-authored-by: Wei Tang <wei@that.world>

--- template/runtime/src/lib.rs
+++ template/runtime/src/lib.rs
@@ -292,6 +292,9 @@ impl pallet_evm::Config for Runtime {
 		pallet_evm_precompile_simple::Sha256,
 		pallet_evm_precompile_simple::Ripemd160,
 		pallet_evm_precompile_simple::Identity,
+		pallet_evm_precompile_simple::ECRecoverPublicKey,
+		pallet_evm_precompile_sha3fips::Sha3FIPS256,
+		pallet_evm_precompile_sha3fips::Sha3FIPS512,
 	);
 	type ChainId = ChainId;
 	type OnChargeTransaction = ();
