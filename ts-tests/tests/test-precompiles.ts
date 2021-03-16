809ba26b244e4130cbdc4f8abaaedc76c4ecbd7c
Wei Tang <accounts@that.world>
2021-03-16 07:07:07 +0100

    Uncomment tests

--- ts-tests/tests/test-precompiles.ts
+++ ts-tests/tests/test-precompiles.ts
@@ -30,43 +30,44 @@ describeWithFrontier("Frontier RPC (Precompile)", `simple-specs.json`, (context)
 		// ensure native web3 sending works as well as truffle provider
 		web3.eth.accounts.wallet.add(GENESIS_ACCOUNT_PRIVATE_KEY);
 		web3.eth.defaultAccount = web3.eth.accounts.wallet[0].address;
-	});	
+	});
+	
 	// Those test are ordered. In general this should be avoided, but due to the time it takes
 	// to spin up a frontier node, it saves a lot of time.
 
-	// it.only('should perform ecrecover', async () => {
-	// 	const web3 = context.web3;
+	it.only('should perform ecrecover', async () => {
+		const web3 = context.web3;
 
-	// 	const message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tubulum fuisse, qua illum, cuius is condemnatus est rogatione, P. Eaedem res maneant alio modo.'
-	// 	const messageHex = '0x' + Buffer.from(message).toString('hex');
-	// 	const sig = (await web3.eth.sign(messageHex, GENESIS_ACCOUNT)).slice(2);
-	// 	const r = `${sig.slice(0, 64)}`
-	// 	const s = `${sig.slice(64, 128)}`
-	// 	const v = `${sig.slice(128, 130)}`
-	// 	const sigPart = `${Buffer.alloc(31).toString('hex')}${v}${r}${s}`;
-	// 	const hash = web3.utils.sha3('\x19Ethereum Signed Message:\n' + message.length + message).slice(2);
-	// 	// const tx = await context.web3.eth.accounts.signTransaction(
-	// 	// 	{
-	// 	// 		from: GENESIS_ACCOUNT,
-	// 	// 		to: ECRECOVER_PRECOMPILE_ADDRESS,
-	// 	// 		data: `0x${hash.toString()}${sigPart}`,
-	// 	// 		value: "0x00",
-	// 	// 		gasPrice: "0x01",
-	// 	// 		gas: "0x100000",
-	// 	// 	},
-	// 	// 	GENESIS_ACCOUNT_PRIVATE_KEY
-	// 	// );
+		const message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tubulum fuisse, qua illum, cuius is condemnatus est rogatione, P. Eaedem res maneant alio modo.'
+		const messageHex = '0x' + Buffer.from(message).toString('hex');
+		const sig = (await web3.eth.sign(messageHex, GENESIS_ACCOUNT)).slice(2);
+		const r = `${sig.slice(0, 64)}`
+		const s = `${sig.slice(64, 128)}`
+		const v = `${sig.slice(128, 130)}`
+		const sigPart = `${Buffer.alloc(31).toString('hex')}${v}${r}${s}`;
+		const hash = web3.utils.sha3('\x19Ethereum Signed Message:\n' + message.length + message).slice(2);
+		const tx = await context.web3.eth.accounts.signTransaction(
+			{
+				from: GENESIS_ACCOUNT,
+				to: ECRECOVER_PRECOMPILE_ADDRESS,
+				data: `0x${hash.toString()}${sigPart}`,
+				value: "0x00",
+				gasPrice: "0x01",
+				gas: "0x100000",
+			},
+			GENESIS_ACCOUNT_PRIVATE_KEY
+		);
 
-	// 	const contract = new context.web3.eth.Contract([{ ...TEST_CONTRACT_ABI[0], inputs: [] }], FIRST_CONTRACT_ADDRESS, {
-	// 		from: GENESIS_ACCOUNT,
-	// 		gasPrice: "0x01",
-	// 	});
-	// 	let result = await contract.methods
-	// 		.ecrecover()
-	// 		.call(`0x${hash.toString()}${sigPart}`)
-	// 	console.log(result);
-	// 	// let result = await customRequest(context.web3, "eth_sendRawTransaction", [tx.rawTransaction]);
-	// });
+		const contract = new context.web3.eth.Contract([{ ...TEST_CONTRACT_ABI[0], inputs: [] }], FIRST_CONTRACT_ADDRESS, {
+			from: GENESIS_ACCOUNT,
+			gasPrice: "0x01",
+		});
+		let result = await contract.methods
+			.ecrecover()
+			.call(`0x${hash.toString()}${sigPart}`)
+		console.log(result);
+		let result = await customRequest(context.web3, "eth_sendRawTransaction", [tx.rawTransaction]);
+	});
 
 	it('should perform identity directly', async () => {
 		const message = '0x1234567890'
