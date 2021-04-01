30c5cf9e49e9d27e4ec1348b56372139a19d501a
tgmichel <telmo@purestake.com>
2021-04-01 12:10:39 +0200

    Fix checker

--- ts-tests/tests/test-contract-storage.ts
+++ ts-tests/tests/test-contract-storage.ts
@@ -12,9 +12,9 @@ describeWithFrontier("Frontier RPC (Contract)", `simple-specs.json`, (context) =
 	const TEST_CONTRACT_ABI = Test.abi as AbiItem[];
 	const TEST_CONTRACT_DEPLOYED_BYTECODE = Test.deployedBytecode;
 
-    it("eth_getStorageAt", async function () {
-        
-        const contract = new context.web3.eth.Contract(TEST_CONTRACT_ABI);
+	it("eth_getStorageAt", async function () {
+		
+		const contract = new context.web3.eth.Contract(TEST_CONTRACT_ABI);
 
 		this.timeout(15000);
 		const tx = await context.web3.eth.accounts.signTransaction(
@@ -28,53 +28,53 @@ describeWithFrontier("Frontier RPC (Contract)", `simple-specs.json`, (context) =
 			GENESIS_ACCOUNT_PRIVATE_KEY
 		);
 
-        expect(await customRequest(context.web3, "eth_sendRawTransaction", [tx.rawTransaction])).to.include({
+		expect(await customRequest(context.web3, "eth_sendRawTransaction", [tx.rawTransaction])).to.include({
 			id: 1,
 			jsonrpc: "2.0",
 		});
 
-        await createAndFinalizeBlock(context.web3);
-        let receipt0 = await context.web3.eth.getTransactionReceipt(tx.transactionHash);
-        let contractAddress = receipt0.contractAddress;
+		await createAndFinalizeBlock(context.web3);
+		let receipt0 = await context.web3.eth.getTransactionReceipt(tx.transactionHash);
+		let contractAddress = receipt0.contractAddress;
 
-        let getStorage0 = await customRequest(context.web3, "eth_getStorageAt", [
-            contractAddress,
-            "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc",
-            "latest",
-        ]);
-      
-        expect(getStorage0.result).to.be.eq(
-            "0x0000000000000000000000000000000000000000000000000000000000000000"
-        );
-      
-        const tx1 = await context.web3.eth.accounts.signTransaction(
-            {
-                from: GENESIS_ACCOUNT,
-                to: contractAddress,
-                data: contract.methods
-                    .setStorage(
-                        "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc",
-                        "0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-                    ).encodeABI(),
-                value: "0x00",
-                gasPrice: "0x01",
-                gas: "0x500000",
-            },
-            GENESIS_ACCOUNT_PRIVATE_KEY
-        );
-      
-        await customRequest(context.web3, "eth_sendRawTransaction", [tx1.rawTransaction]);
-        await createAndFinalizeBlock(context.web3);
-        let receip1 = await context.web3.eth.getTransactionReceipt(tx1.transactionHash);
-    
-        let getStorage1 = await customRequest(context.web3, "eth_getStorageAt", [
-            contractAddress,
-            "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc",
-            "latest",
-        ]);
-    
-        expect(getStorage1.result).to.be.eq(
-            "0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
-        );
-    });
+		let getStorage0 = await customRequest(context.web3, "eth_getStorageAt", [
+			contractAddress,
+			"0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc",
+			"latest",
+		]);
+	
+		expect(getStorage0.result).to.be.eq(
+			"0x0000000000000000000000000000000000000000000000000000000000000000"
+		);
+	
+		const tx1 = await context.web3.eth.accounts.signTransaction(
+			{
+				from: GENESIS_ACCOUNT,
+				to: contractAddress,
+				data: contract.methods
+					.setStorage(
+						"0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc",
+						"0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+					).encodeABI(),
+				value: "0x00",
+				gasPrice: "0x01",
+				gas: "0x500000",
+			},
+			GENESIS_ACCOUNT_PRIVATE_KEY
+		);
+	
+		await customRequest(context.web3, "eth_sendRawTransaction", [tx1.rawTransaction]);
+		await createAndFinalizeBlock(context.web3);
+		let receip1 = await context.web3.eth.getTransactionReceipt(tx1.transactionHash);
+	
+		let getStorage1 = await customRequest(context.web3, "eth_getStorageAt", [
+			contractAddress,
+			"0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc",
+			"latest",
+		]);
+	
+		expect(getStorage1.result).to.be.eq(
+			"0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+		);
+	});
 });
