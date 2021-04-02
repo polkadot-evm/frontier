4190349086e2e520a25b290efc6377ed1e98686e
Stephen Shelton <steve@brewcraft.org>
2021-04-02 07:40:27 -0600

    Include Modexp in precompile set

--- ts-tests/tests/test-precompile-modexp-eip2565.ts
+++ ts-tests/tests/test-precompile-modexp-eip2565.ts
@@ -9,6 +9,8 @@ const GENESIS_ACCOUNT = "0x6be02d1d3665660d22ff9624b7be0551ee1ac91b";
 const GENESIS_ACCOUNT_PRIVATE_KEY =
     "0x99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342";
 
+const MODEXP_CONTRACT_ADDRESS = "0000000000000000000000000000000000000005";
+
 function readFile(path) {
 	return new Promise<string>((resolve, reject) => {
 		fs.readFile(path, "utf8", (err, data) => {
