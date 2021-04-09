3ec58445cdaf0c951529d72d28a3cb6e25162ad9
Wei Tang <wei@that.world>
2021-04-09 14:49:39 +0200

    ts-tests: switch to use builtin dev config directly (#353)
    * ts-tests: switch to use builtin dev config directly
    
    * Add evm balance for dev config

--- ts-tests/tests/test-bloom.ts
+++ ts-tests/tests/test-bloom.ts
@@ -3,7 +3,7 @@ import { step } from "mocha-steps";
 
 import { createAndFinalizeBlock, customRequest, describeWithFrontier } from "./util";
 
-describeWithFrontier("Frontier RPC (Bloom)", `simple-specs.json`, (context) => {
+describeWithFrontier("Frontier RPC (Bloom)", (context) => {
 
 	const GENESIS_ACCOUNT = "0x6be02d1d3665660d22ff9624b7be0551ee1ac91b";
 	const GENESIS_ACCOUNT_PRIVATE_KEY = "0x99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342";
--- ts-tests/tests/test-gas.ts
+++ ts-tests/tests/test-gas.ts
@@ -4,7 +4,7 @@ import Test from "../build/contracts/Test.json"
 import { describeWithFrontier, createAndFinalizeBlock } from "./util";
 import { AbiItem } from "web3-utils";
 
-describeWithFrontier("Frontier RPC (Gas)", `simple-specs.json`, (context) => {
+describeWithFrontier("Frontier RPC (Gas)", (context) => {
 	const GENESIS_ACCOUNT = "0x6be02d1d3665660d22ff9624b7be0551ee1ac91b";
 
 	const TEST_CONTRACT_BYTECODE = Test.bytecode;
