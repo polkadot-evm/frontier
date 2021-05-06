3aa29c2475b85200faab59a3d911fc99472ce524
Crystalin <alan@purestake.com>
2021-05-06 01:26:30 +0000

    Fix balance check in evm

--- frame/evm/src/lib.rs
+++ frame/evm/src/lib.rs
@@ -643,8 +643,7 @@ where
 			// merge the imbalance caused by paying the fees and refunding parts of it again.
 			let adjusted_paid = paid
 				.offset(refund_imbalance)
-				//Peer Review: Is this right? https://crates.parity.io/frame_support/traits/enum.SameOrOther.html
-				.try_same()
+				.same()
 				.map_err(|_| Error::<T>::BalanceLow)?;
 			OU::on_unbalanced(adjusted_paid);
 		}
