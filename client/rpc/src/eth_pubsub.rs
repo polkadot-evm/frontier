da5b0ddfecbae96d0cf97b72396e3dfa13589ceb
Mr-Leshiy <leshiy12345678@gmail.com>
2021-05-16 21:07:26 +0300

    refactor header size field

--- client/rpc/src/eth_pubsub.rs
+++ client/rpc/src/eth_pubsub.rs
@@ -150,7 +150,7 @@ impl SubscriptionResult {
 						)
 					],
 					size: Some(U256::from(
-						rlp::encode(&block).len() as u32
+						rlp::encode(&block.header).len() as u32
 					)),
 				},
 				extra_info: BTreeMap::new()
