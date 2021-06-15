a41ca3bed037a494bbd51f16059cbae8fa43daa5
Stephen Shelton <steve@brewcraft.org>
2021-06-15 06:58:09 -0600

    Add back mock runtime, start funding account work

--- frame/evm/src/benchmarks.rs
+++ frame/evm/src/benchmarks.rs
@@ -19,20 +19,21 @@
 //! Benchmarking
 use sp_std::prelude::*;
 use crate::{Config, Pallet, EnsureAddressNever, EnsureAddressSame, EnsureAddressRoot,
-	FeeCalculator, HashedAddressMapping, Event};
+	FeeCalculator, HashedAddressMapping, Event, BalanceOf, AddressMapping};
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, account};
 use frame_system::RawOrigin;
 use frame_support::{
 	assert_ok, impl_outer_origin, parameter_types, impl_outer_dispatch,
 };
+use frame_support::traits::Currency;
 use sp_runtime::{
+	generic,
     traits::{BlakeTwo256, IdentityLookup},
     Perbill,
 };
 use sp_core::{U256, H256, H160, crypto::AccountId32};
 use sp_std::boxed::Box;
 
-/*
 impl_outer_origin! {
 	pub enum Origin for Test where system = frame_system {}
 }
@@ -77,7 +78,7 @@ impl frame_system::Config for Test {
 	type Hashing = BlakeTwo256;
 	type AccountId = AccountId32;
 	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
+	type Header = generic::Header<u64, BlakeTwo256>;
 	type Event = ();
 	type BlockHashCount = BlockHashCount;
 	type Version = ();
@@ -91,7 +92,7 @@ impl frame_system::Config for Test {
 }
 
 parameter_types! {
-	pub const ExistentialDeposit: u64 = 1;
+	pub const ExistentialDeposit: u64 = 0;
 }
 impl pallet_balances::Config for Test {
 	type MaxLocks = ();
@@ -127,6 +128,16 @@ type System = frame_system::Module<Test>;
 type Balances = pallet_balances::Module<Test>;
 // type EVM = Module<Test>;
 
+pub struct MockedAddressMapping;
+
+impl AddressMapping<AccountId32> for MockedAddressMapping {
+    fn into_account_id(address: H160) -> AccountId32 {
+        let mut data = [0u8; 32];
+        data[0..20].copy_from_slice(&address[..]);
+        AccountId32::from(Into::<[u8; 32]>::into(data))
+    }
+}
+
 impl Config for Test {
 	type FeeCalculator = FixedGasPrice;
 	type GasWeightMapping = ();
@@ -134,7 +145,7 @@ impl Config for Test {
 	type CallOrigin = EnsureAddressRoot<Self::AccountId>;
 	type WithdrawOrigin = EnsureAddressNever<Self::AccountId>;
 
-	type AddressMapping = HashedAddressMapping<Blake2Hasher>;
+	type AddressMapping = MockedAddressMapping;
 	type Currency = Balances;
 	type Runner = crate::runner::stack::Runner<Self>;
 
@@ -144,7 +155,18 @@ impl Config for Test {
 	type BlockGasLimit = ();
 	type OnChargeTransaction = ();
 }
-*/
+
+fn create_funded_user<T: Config>(
+	string: &'static str,
+	n: u32,
+	balance: BalanceOf<T>,
+) -> T::AccountId {
+	const SEED: u32 = 0;
+	let user = account(string, n, SEED);
+	T::Currency::make_free_balance_be(&user, balance);
+	T::Currency::issue(balance);
+	user
+}
 
 benchmarks! {
 	test {
@@ -152,10 +174,25 @@ benchmarks! {
 		// XXX: remove, seems to throw off macro if not present
 		let x in 1..1_000_000_000;
 
+		let caller: AccountId32 = create_funded_user::<Test>("caller", 0, 1000000000000000000u64.into());
+		// let bytes: [u8; 20] = AsRef::<[u8; 32]>::as_ref(&caller)[0..20];
+		let caller_address = H160::from_slice(&AsRef::<[u8; 32]>::as_ref(&caller)[0..20]); // WTF
+		// let caller_address = H160::from_slice(caller.as_ref()[0..20]);
+
+		// let caller_address = H160::from_slice(caller.as_ref()[0..20]);
+		// let caller_address = H160::from(AsRef::<[u8; 32]>::as_ref(&caller)[0..20]); // WTF
+		// let caller_address = H160::from(caller.as_byte_slice()[0..20]);
+		/*
+		let bytes: [u8] = caller.as_slice_of::<u8>().expect();
+		let caller = H160::from(bytes[0..20]);
+		T::AccountId::decode(&mut &bytes[..]).unwrap_or_default();
+		*/
+
+
 		let contract_bytecode = vec![96, 128, 96, 64, 82, 52, 128, 21, 97, 0, 16, 87, 96, 0, 128, 253, 91, 80, 97, 2, 43, 128, 97, 0, 32, 96, 0, 57, 96, 0, 243, 254, 96, 128, 96, 64, 82, 52, 128, 21, 97, 0, 16, 87, 96, 0, 128, 253, 91, 80, 96, 4, 54, 16, 97, 0, 43, 87, 96, 0, 53, 96, 224, 28, 128, 99, 15, 20, 164, 6, 20, 97, 0, 48, 87, 91, 96, 0, 128, 253, 91, 97, 0, 74, 96, 4, 128, 54, 3, 129, 1, 144, 97, 0, 69, 145, 144, 97, 0, 179, 86, 91, 97, 0, 96, 86, 91, 96, 64, 81, 97, 0, 87, 145, 144, 97, 0, 235, 86, 91, 96, 64, 81, 128, 145, 3, 144, 243, 91, 96, 0, 128, 96, 0, 144, 80, 96, 0, 91, 131, 129, 16, 21, 97, 0, 148, 87, 96, 1, 130, 97, 0, 127, 145, 144, 97, 1, 6, 86, 91, 145, 80, 128, 128, 97, 0, 140, 144, 97, 1, 102, 86, 91, 145, 80, 80, 97, 0, 106, 86, 91, 80, 128, 145, 80, 80, 145, 144, 80, 86, 91, 96, 0, 129, 53, 144, 80, 97, 0, 173, 129, 97, 1, 222, 86, 91, 146, 145, 80, 80, 86, 91, 96, 0, 96, 32, 130, 132, 3, 18, 21, 97, 0, 197, 87, 96, 0, 128, 253, 91, 96, 0, 97, 0, 211, 132, 130, 133, 1, 97, 0, 158, 86, 91, 145, 80, 80, 146, 145, 80, 80, 86, 91, 97, 0, 229, 129, 97, 1, 92, 86, 91, 130, 82, 80, 80, 86, 91, 96, 0, 96, 32, 130, 1, 144, 80, 97, 1, 0, 96, 0, 131, 1, 132, 97, 0, 220, 86, 91, 146, 145, 80, 80, 86, 91, 96, 0, 97, 1, 17, 130, 97, 1, 92, 86, 91, 145, 80, 97, 1, 28, 131, 97, 1, 92, 86, 91, 146, 80, 130, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 3, 130, 17, 21, 97, 1, 81, 87, 97, 1, 80, 97, 1, 175, 86, 91, 91, 130, 130, 1, 144, 80, 146, 145, 80, 80, 86, 91, 96, 0, 129, 144, 80, 145, 144, 80, 86, 91, 96, 0, 97, 1, 113, 130, 97, 1, 92, 86, 91, 145, 80, 127, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 130, 20, 21, 97, 1, 164, 87, 97, 1, 163, 97, 1, 175, 86, 91, 91, 96, 1, 130, 1, 144, 80, 145, 144, 80, 86, 91, 127, 78, 72, 123, 113, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 96, 0, 82, 96, 17, 96, 4, 82, 96, 36, 96, 0, 253, 91, 97, 1, 231, 129, 97, 1, 92, 86, 91, 129, 20, 97, 1, 242, 87, 96, 0, 128, 253, 91, 80, 86, 254, 162, 100, 105, 112, 102, 115, 88, 34, 18, 32, 136, 0, 161, 138, 68, 107, 43, 75, 137, 142, 85, 33, 158, 30, 40, 248, 147, 37, 182, 225, 205, 14, 92, 87, 133, 97, 3, 76, 150, 37, 152, 138, 100, 115, 111, 108, 99, 67, 0, 8, 2, 0, 51];
 
-		let source_addr_bytes = vec![238, 139, 183, 163, 132, 239, 34, 101, 124, 51, 180, 96, 215, 171, 66, 56, 131, 9, 97, 55];
-		let source = H160::from_slice(source_addr_bytes.as_slice());
+		// let source_addr_bytes = vec![238, 139, 183, 163, 132, 239, 34, 101, 124, 51, 180, 96, 215, 171, 66, 56, 131, 9, 97, 55];
+		// let source = H160::from_slice(source_addr_bytes.as_slice());
 
 		let nonce: u64 = 0;
 		let nonce_as_u256: U256 = nonce.into();
@@ -165,7 +202,7 @@ benchmarks! {
 		let gas_price = 1000000000.into();
 		let create_dispatch_results = Pallet::<T>::create(
 			RawOrigin::Root.into(),
-			source,
+			caller_address,
 			contract_bytecode,
 			value,
 			gas_limit,
@@ -186,7 +223,7 @@ benchmarks! {
 
 		let call_dispatch_results = Pallet::<T>::call(
 			RawOrigin::Root.into(),
-			source,
+			caller_address,
 			contract_address,
 			encoded_call,
 			value,
