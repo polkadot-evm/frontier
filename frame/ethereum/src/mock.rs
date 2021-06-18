c5f9f4c4ba84a12b24bddc56a307b7c1b80360bc
tgmichel <telmo@purestake.com>
2021-06-18 12:45:15 +0200

    Update pallet-ethereum test

--- frame/ethereum/src/mock.rs
+++ frame/ethereum/src/mock.rs
@@ -158,6 +158,7 @@ impl pallet_evm::Config for Test {
 	type ChainId = ChainId;
 	type BlockGasLimit = BlockGasLimit;
 	type OnChargeTransaction = ();
+	type BlockHashMapping = Ethereum;
 }
 
 impl Config for Test {
