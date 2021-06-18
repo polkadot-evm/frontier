8cefb6fbdfbb06d3eb4312b168ca3c12976f8860
Stephen Shelton <steve@brewcraft.org>
2021-06-18 10:03:27 -0600

    Revert changes picked up from previous starting branch

--- client/consensus/src/lib.rs
+++ client/consensus/src/lib.rs
@@ -103,7 +103,6 @@ impl<B, I, C> FrontierBlockImport<B, I, C> where
 	}
 }
 
-#[async_trait::async_trait]
 impl<B, I, C> BlockImport<B> for FrontierBlockImport<B, I, C> where
 	B: BlockT,
 	I: BlockImport<B, Transaction = sp_api::TransactionFor<C, B>> + Send + Sync,
@@ -115,14 +114,14 @@ impl<B, I, C> BlockImport<B> for FrontierBlockImport<B, I, C> where
 	type Error = ConsensusError;
 	type Transaction = sp_api::TransactionFor<C, B>;
 
-	async fn check_block(
+	fn check_block(
 		&mut self,
 		block: BlockCheckParams<B>,
 	) -> Result<ImportResult, Self::Error> {
 		self.inner.check_block(block).await.map_err(Into::into)
 	}
 
-	async fn import_block(
+	fn import_block(
 		&mut self,
 		block: BlockImportParams<B, Self::Transaction>,
 		new_cache: HashMap<CacheKeyId, Vec<u8>>,
@@ -132,6 +131,6 @@ impl<B, I, C> BlockImport<B> for FrontierBlockImport<B, I, C> where
 		// worker.
 		ensure_log(&block.header.digest()).map_err(|e| Error::from(e))?;
 
-		self.inner.import_block(block, new_cache).await.map_err(Into::into)
+		self.inner.import_block(block, new_cache).map_err(Into::into)
 	}
 }
