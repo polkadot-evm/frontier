41326e6ef8db639aa2f9a14aaf13be8fa35c5889
Stephen Shelton <steve@brewcraft.org>
2021-06-18 10:17:53 -0600

    Other minor clean up

--- frame/evm/src/lib.rs
+++ frame/evm/src/lib.rs
@@ -53,7 +53,7 @@
 // Ensure we're `no_std` when compiling for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub mod tests;
+mod tests;
 pub mod runner;
 
 // #[cfg(feature = "runtime-benchmarks")]
