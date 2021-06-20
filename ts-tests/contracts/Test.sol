7fa294ecd3fc7aa1e752014a95811785cc67be79
tgmichel <telmo@purestake.com>
2021-06-20 22:38:54 +0200

    Add `pallet_evm::Config::BlockHashMapping` (#409)
    * Add `pallet_evm::Config::BlockHashMapping`
    
    * `BlockHashCount` solidity compliance
    
    * `pallet-evm` 5.0.0
    
    * Fix evm version
    
    * Add ts test (wip)
    
    * CHANGELOG
    
    * Specify prerelease version
    
    * Cargo.lock
    
    * Fix ts test
    
    * Versioning
    
    * Update pallet-evm test
    
    * Update pallet-ethereum test
    
    * Update test gas
    
    * Remove version
    
    * Implement `BlockHashMapping` for struct instead for `Module`
    
    * Not generic
    
    * `pallet-dynamic-fee` 3.0.0-dev
    
    * Remove version
    
    * Cargo.lock

--- ts-tests/contracts/Test.sol
+++ ts-tests/contracts/Test.sol
@@ -4,7 +4,10 @@ contract Test {
     function multiply(uint a) public pure returns(uint d) {
         return a * 7;
     }
-    function currentBlock() public view  returns(uint) {
+    function currentBlock() public view returns(uint) {
         return block.number;
+    }
+    function blockHash(uint number) public view returns(bytes32) {
+        return blockhash(number);
     }
 }
