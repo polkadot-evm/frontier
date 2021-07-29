1433758edaba719d7fcc7a86c1142265e1e8b8b5
sorpaas <sorpaas@users.noreply.github.com>
2021-07-29 21:44:48 +0000

    deploy: 87bbfe205eb6c7677ca1a0060e6362a4738e0541

--- rustdocs/fc_mapping_sync/sidebar-items.js
+++ rustdocs/fc_mapping_sync/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["sync_block",""],["sync_blocks",""],["sync_genesis_block",""],["sync_one_block",""]],"struct":[["MappingSyncWorker",""]]});
+initSidebarItems({"enum":[["SyncStrategy",""]],"fn":[["sync_block",""],["sync_blocks",""],["sync_genesis_block",""],["sync_one_block",""]],"struct":[["MappingSyncWorker",""]]});
