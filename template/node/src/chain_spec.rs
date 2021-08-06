32e69e8511ace2683d58218b69c8cdfdd32254db
Dan Shields <35669742+NukeManDan@users.noreply.github.com>
2021-08-06 16:19:32 -0600

    Update Template use and utils (#437)
    * update template readme w/ generation instructions.
    
    * update example
    
    * npm audit fix
    
    * update `Alice` genisis account address
    
    * update erc20 example
    
    * spelling
    
    * remove unused import, update readme, tested to work!
    
    * npm audit fix tests
    
    * add CI testing account to genesis with balance

--- template/node/src/chain_spec.rs
+++ template/node/src/chain_spec.rs
@@ -165,6 +165,23 @@ fn testnet_genesis(
 			accounts: {
 				let mut map = BTreeMap::new();
 				map.insert(
+					// H160 address of Alice dev account
+					// Derived from SS58 (42 prefix) address
+					// SS58: 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
+					// hex: 0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d
+					// Using the full hex key, truncating to the first 20 bytes (the first 40 hex chars)
+					H160::from_str("d43593c715fdd31c61141abd04a99fd6822c8558")
+						.expect("internal H160 is valid; qed"),
+					pallet_evm::GenesisAccount {
+						balance: U256::from_str("0xffffffffffffffffffffffffffffffff")
+							.expect("internal U256 is valid; qed"),
+						code: Default::default(),
+						nonce: Default::default(),
+						storage: Default::default(),
+					},
+				);
+				map.insert(
+					// H160 address of CI test runner account
 					H160::from_str("6be02d1d3665660d22ff9624b7be0551ee1ac91b")
 						.expect("internal H160 is valid; qed"),
 					pallet_evm::GenesisAccount {
