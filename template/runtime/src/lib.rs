235b9d1a56e7dac0fa0cdfe7ec96b5d4a0251f58
Wei Tang <wei@that.world>
2021-08-18 11:39:37 +0000

    Change BaseCallFilter to Everything

--- template/runtime/src/lib.rs
+++ template/runtime/src/lib.rs
@@ -143,7 +143,7 @@ parameter_types! {
 
 impl frame_system::Config for Runtime {
 	/// The basic call filter to use in dispatchable.
-	type BaseCallFilter = ();
+	type BaseCallFilter = frame_support::traits::Everything;
 	/// Block & extrinsics weights: base values and limits.
 	type BlockWeights = BlockWeights;
 	/// The maximum length of a block (in bytes).
