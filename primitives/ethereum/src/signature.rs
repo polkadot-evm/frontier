3d086337ede900abb2b94e4f34cfe26024837f16
Wei Tang <wei@that.world>
2021-09-27 16:09:36 +0000

    Signed custom extrinsic implementation

---
+++ primitives/ethereum/src/signature.rs
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: Apache-2.0
+// This file is part of Frontier.
+//
+// Copyright (c) 2020 Parity Technologies (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use codec::{Decode, Encode};
+#[cfg(feature = "std")]
+use serde::{Serialize, Deserialize};
+use scale_info::TypeInfo;
+use core::convert::TryFrom;
+use sp_core::{crypto::Public, H256, ecdsa, ed25519, sr25519};
+use sp_runtime::{RuntimeDebug, AccountId32, MultiSigner, traits::{Verify, Lazy}};
+
+/// Signature verify that can work with any known signature types.
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(Eq, PartialEq, Clone, Encode, Decode, RuntimeDebug, TypeInfo)]
+pub enum MultiSignature {
+	/// An Ed25519 signature.
+	Ed25519(ed25519::Signature),
+	/// An Sr25519 signature.
+	Sr25519(sr25519::Signature),
+	/// An ECDSA/SECP256k1 signature.
+	Ecdsa(ecdsa::Signature),
+	/// A pre-hashed ECDSA/SECP256k1 signature.
+	EthereumTransaction(H256, ecdsa::Signature),
+}
+
+impl From<ed25519::Signature> for MultiSignature {
+	fn from(x: ed25519::Signature) -> Self {
+		Self::Ed25519(x)
+	}
+}
+
+impl TryFrom<MultiSignature> for ed25519::Signature {
+	type Error = ();
+	fn try_from(m: MultiSignature) -> Result<Self, Self::Error> {
+		if let MultiSignature::Ed25519(x) = m {
+			Ok(x)
+		} else {
+			Err(())
+		}
+	}
+}
+
+impl From<sr25519::Signature> for MultiSignature {
+	fn from(x: sr25519::Signature) -> Self {
+		Self::Sr25519(x)
+	}
+}
+
+impl TryFrom<MultiSignature> for sr25519::Signature {
+	type Error = ();
+	fn try_from(m: MultiSignature) -> Result<Self, Self::Error> {
+		if let MultiSignature::Sr25519(x) = m {
+			Ok(x)
+		} else {
+			Err(())
+		}
+	}
+}
+
+impl From<ecdsa::Signature> for MultiSignature {
+	fn from(x: ecdsa::Signature) -> Self {
+		Self::Ecdsa(x)
+	}
+}
+
+impl TryFrom<MultiSignature> for ecdsa::Signature {
+	type Error = ();
+	fn try_from(m: MultiSignature) -> Result<Self, Self::Error> {
+		if let MultiSignature::Ecdsa(x) = m {
+			Ok(x)
+		} else {
+			Err(())
+		}
+	}
+}
+
+impl Default for MultiSignature {
+	fn default() -> Self {
+		Self::Ed25519(Default::default())
+	}
+}
+
+impl Verify for MultiSignature {
+	type Signer = MultiSigner;
+	fn verify<L: Lazy<[u8]>>(&self, mut msg: L, signer: &AccountId32) -> bool {
+		match (self, signer) {
+			(Self::Ed25519(ref sig), who) =>
+				sig.verify(msg, &ed25519::Public::from_slice(who.as_ref())),
+			(Self::Sr25519(ref sig), who) =>
+				sig.verify(msg, &sr25519::Public::from_slice(who.as_ref())),
+			(Self::Ecdsa(ref sig), who) => {
+				let m = sp_io::hashing::blake2_256(msg.get());
+				match sp_io::crypto::secp256k1_ecdsa_recover_compressed(sig.as_ref(), &m) {
+					Ok(pubkey) =>
+						&sp_io::hashing::blake2_256(pubkey.as_ref()) ==
+							<dyn AsRef<[u8; 32]>>::as_ref(who),
+					_ => false,
+				}
+			},
+			// No AccountId32 signature is valid for this type.
+			(Self::EthereumTransaction(_, _), _) => false,
+		}
+	}
+}
---
+++ primitives/ethereum/src/unchecked_extrinsic.rs
@@ -0,0 +1,117 @@
+use scale_info::TypeInfo;
+use codec::Encode;
+use sp_core::{H160, H256};
+use sp_runtime::{
+	traits::{
+		self, Checkable, Extrinsic, ExtrinsicMetadata, IdentifyAccount, MaybeDisplay, Member,
+		SignedExtension,
+	},
+	transaction_validity::{InvalidTransaction, TransactionValidityError},
+};
+use sha3::{Digest, Keccak256};
+use crate::{CheckedExtrinsic, CheckedSignature, MultiSignature, EthereumAddress};
+
+/// A extrinsic right from the external world. This is unchecked and so
+/// can contain a signature.
+#[derive(PartialEq, Eq, Clone, TypeInfo)]
+pub struct UncheckedExtrinsic<Address, Call, Extra: SignedExtension>(
+	sp_runtime::generic::UncheckedExtrinsic<Address, Call, MultiSignature, Extra>,
+);
+
+#[cfg(feature = "std")]
+impl<Address, Call, Extra> parity_util_mem::MallocSizeOf
+	for UncheckedExtrinsic<Address, Call, Extra>
+where
+	Extra: SignedExtension,
+{
+	fn size_of(&self, ops: &mut parity_util_mem::MallocSizeOfOps) -> usize {
+		self.0.size_of(ops)
+	}
+}
+
+impl<Address, Call, Extra: SignedExtension>
+	UncheckedExtrinsic<Address, Call, Extra>
+{
+	/// New instance of a signed extrinsic aka "transaction".
+	pub fn new_signed(function: Call, signed: Address, signature: MultiSignature, extra: Extra) -> Self {
+		Self(sp_runtime::generic::UncheckedExtrinsic::new_signed(function, signed, signature, extra))
+	}
+
+	/// New instance of an unsigned extrinsic aka "inherent".
+	pub fn new_unsigned(function: Call) -> Self {
+		Self(sp_runtime::generic::UncheckedExtrinsic::new_unsigned(function))
+	}
+}
+
+impl<Address, Call, Extra: SignedExtension> Extrinsic
+	for UncheckedExtrinsic<Address, Call, Extra>
+{
+	type Call = Call;
+
+	type SignaturePayload = (Address, MultiSignature, Extra);
+
+	fn is_signed(&self) -> Option<bool> {
+		self.0.is_signed()
+	}
+
+	fn new(function: Call, signed_data: Option<Self::SignaturePayload>) -> Option<Self> {
+		sp_runtime::generic::UncheckedExtrinsic::new(function, signed_data).map(Self)
+	}
+}
+
+impl<Address, AccountId, Call, Extra, Lookup> Checkable<Lookup>
+	for UncheckedExtrinsic<Address, Call, Extra>
+where
+	Address: Member + MaybeDisplay + EthereumAddress,
+	Call: Encode + Member,
+	MultiSignature: Member + traits::Verify,
+	<MultiSignature as traits::Verify>::Signer: IdentifyAccount<AccountId = AccountId>,
+	Extra: SignedExtension<AccountId = AccountId>,
+	AccountId: Member + MaybeDisplay,
+	Lookup: traits::Lookup<Source = Address, Target = AccountId>,
+{
+	type Checked = CheckedExtrinsic<AccountId, Call, Extra>;
+
+	fn check(self, lookup: &Lookup) -> Result<Self::Checked, TransactionValidityError> {
+		let ethereum_address = self.0.signature.as_ref().and_then(|(address, _, _)| address.ethereum_address());
+
+		match (ethereum_address, self.0) {
+			(Some(ethereum_address), sp_runtime::generic::UncheckedExtrinsic {
+				signature: Some((_, MultiSignature::EthereumTransaction(preimage_hash, signature), _)),
+				function,
+			}) => {
+				let recovered_pubkey = sp_io::crypto::secp256k1_ecdsa_recover(&signature.0, &preimage_hash.0).map_err(|_| TransactionValidityError::Invalid(InvalidTransaction::BadProof))?;
+				let recovered_address = H160::from(H256::from_slice(
+					Keccak256::digest(&recovered_pubkey).as_slice(),
+				));
+				if recovered_address == ethereum_address && Extra::metadata().is_empty() {
+					Ok(CheckedExtrinsic {
+						signed: CheckedSignature::EthereumTransaction(preimage_hash, ethereum_address),
+						function,
+					})
+				} else {
+					Err(TransactionValidityError::Invalid(InvalidTransaction::BadProof))
+				}
+			},
+			(_, extrinsic) => {
+				let checked = Checkable::<Lookup>::check(extrinsic, lookup)?;
+				Ok(CheckedExtrinsic {
+					signed: match checked.signed {
+						Some((id, extra)) => CheckedSignature::Signed(id, extra),
+						None => CheckedSignature::Unsigned,
+					},
+					function: checked.function,
+				})
+			},
+		}
+	}
+}
+
+impl<Address, Call, Extra> ExtrinsicMetadata
+	for UncheckedExtrinsic<Address, Call, Extra>
+where
+	Extra: SignedExtension,
+{
+	const VERSION: u8 = <sp_runtime::generic::UncheckedExtrinsic<Address, Call, MultiSignature, Extra> as ExtrinsicMetadata>::VERSION;
+	type SignedExtensions = Extra;
+}
