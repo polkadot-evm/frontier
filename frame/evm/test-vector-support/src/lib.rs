fec0cbc104866c70489f92062083011980449f50
Joshy Orndorff <admin@joshyorndorff.com>
2021-10-01 08:25:04 -0400

    PrecompileOutput in evm test vector support

--- frame/evm/test-vector-support/src/lib.rs
+++ frame/evm/test-vector-support/src/lib.rs
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use evm::{executor::PrecompileOutput, Context, ExitSucceed};
+use evm::{Context, ExitSucceed};
 use fp_evm::Precompile;
 
 #[cfg(feature = "std")]
