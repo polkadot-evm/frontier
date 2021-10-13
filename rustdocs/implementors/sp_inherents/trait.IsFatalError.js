0f071f53cf5ce41c29e894b2e4094f932b28a9d7
sorpaas <sorpaas@users.noreply.github.com>
2021-10-13 05:51:50 +0000

    deploy: 0a8e696fdfb9ce73a7f99941a2f2ec22eefd4f38

--- rustdocs/implementors/sp_inherents/trait.IsFatalError.js
+++ rustdocs/implementors/sp_inherents/trait.IsFatalError.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["pallet_dynamic_fee"] = [{"text":"impl IsFatalError for <a class=\"enum\" href=\"pallet_dynamic_fee/enum.InherentError.html\" title=\"enum pallet_dynamic_fee::InherentError\">InherentError</a>","synthetic":false,"types":["pallet_dynamic_fee::InherentError"]}];
+implementors["pallet_dynamic_fee"] = [{"text":"impl IsFatalError for <a class=\"enum\" href=\"pallet_dynamic_fee/pallet/enum.InherentError.html\" title=\"enum pallet_dynamic_fee::pallet::InherentError\">InherentError</a>","synthetic":false,"types":["pallet_dynamic_fee::pallet::InherentError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
