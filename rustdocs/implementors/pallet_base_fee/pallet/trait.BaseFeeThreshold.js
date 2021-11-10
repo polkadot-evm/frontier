140761318476e3713064b5baf0fab6150b9f5ec3
sorpaas <sorpaas@users.noreply.github.com>
2021-11-10 12:46:02 +0000

    deploy: bcae5695242cc346aa26814b1160fe7e041854b3

---
+++ rustdocs/implementors/pallet_base_fee/pallet/trait.BaseFeeThreshold.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["frontier_template_runtime"] = [{"text":"impl <a class=\"trait\" href=\"pallet_base_fee/pallet/trait.BaseFeeThreshold.html\" title=\"trait pallet_base_fee::pallet::BaseFeeThreshold\">BaseFeeThreshold</a> for <a class=\"struct\" href=\"frontier_template_runtime/struct.BaseFeeThreshold.html\" title=\"struct frontier_template_runtime::BaseFeeThreshold\">BaseFeeThreshold</a>","synthetic":false,"types":["frontier_template_runtime::BaseFeeThreshold"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
