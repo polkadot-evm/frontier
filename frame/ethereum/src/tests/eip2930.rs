660cf8751b367f3268decfb77935c079f634881c
tgmichel <telmo@purestake.com>
2021-12-03 15:45:23 +0100

    Update tests

--- frame/ethereum/src/tests/eip2930.rs
+++ frame/ethereum/src/tests/eip2930.rs
@@ -228,7 +228,7 @@ fn transaction_should_generate_correct_gas_used() {
 	let (pairs, mut ext) = new_test_ext(1);
 	let alice = &pairs[0];
 
-	let expected_gas = U256::from(891328);
+	let expected_gas = U256::from(893928);
 
 	ext.execute_with(|| {
 		let t = eip2930_erc20_creation_transaction(alice);
