210739b001a4d12921f52bf9681d9c84fae3c618
dmitrylavrenov <lawrenowdima@gmail.com>
2022-01-06 17:52:18 +0300

    Remove useless acccount

--- frame/evm/src/backend.rs
+++ frame/evm/src/backend.rs
@@ -28,16 +28,6 @@ use sha3::{Keccak256, Digest};
 use evm::backend::{Backend as BackendT, ApplyBackend, Apply};
 use crate::{AccountStorages, AccountCodes, Config, Event, Pallet};
 
-#[derive(Clone, Eq, PartialEq, Encode, Decode, Default)]
-#[cfg_attr(feature = "std", derive(Debug, Serialize, Deserialize))]
-/// Ethereum account nonce, balance and code. Used by storage.
-pub struct Account {
-	/// Account nonce.
-	pub nonce: U256,
-	/// Account balance.
-	pub balance: U256,
-}
-
 #[derive(Clone, Eq, PartialEq, Encode, Decode)]
 #[cfg_attr(feature = "std", derive(Debug, Serialize, Deserialize))]
 /// Ethereum log. Used for `deposit_event`.
