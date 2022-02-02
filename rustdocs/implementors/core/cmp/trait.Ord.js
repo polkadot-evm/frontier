630ac0a6648fe9297d552abc54fa04c6b9137570
sorpaas <sorpaas@users.noreply.github.com>
2022-02-02 15:56:31 +0000

    deploy: ee8c5b4a0ed24bcc2fa794070e443abbdbc4d6a6

--- rustdocs/implementors/core/cmp/trait.Ord.js
+++ rustdocs/implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["pallet_ethereum"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"pallet_ethereum/enum.EthereumStorageSchema.html\" title=\"enum pallet_ethereum::EthereumStorageSchema\">EthereumStorageSchema</a>","synthetic":false,"types":["pallet_ethereum::EthereumStorageSchema"]}];
+implementors["fp_storage"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"fp_storage/enum.EthereumStorageSchema.html\" title=\"enum fp_storage::EthereumStorageSchema\">EthereumStorageSchema</a>","synthetic":false,"types":["fp_storage::EthereumStorageSchema"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
