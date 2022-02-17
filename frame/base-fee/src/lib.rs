5305107b46ebea0bde00f9b1ae99e3b8ba2a553d
tgmichel <telmo@purestake.com>
2022-02-17 14:40:47 +0100

    fmt

--- frame/base-fee/src/lib.rs
+++ frame/base-fee/src/lib.rs
@@ -18,10 +18,7 @@
 
 pub use pallet::*;
 
-use frame_support::{
-	traits::Get,
-	weights::Weight
-};
+use frame_support::{traits::Get, weights::Weight};
 use sp_core::U256;
 use sp_runtime::Permill;
 
