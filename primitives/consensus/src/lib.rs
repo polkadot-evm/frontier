d564f050dc5924f1d2072c9191fd8668765599b7
yjh <yjh465402634@gmail.com>
2022-02-28 10:33:43 +0800

    chore: simplify find_log (#556)
    * chore: simplify find_log
    
    * fmt
    
    * fmt

--- primitives/consensus/src/lib.rs
+++ primitives/consensus/src/lib.rs
@@ -89,25 +89,21 @@ pub enum FindLogError {
 }
 
 pub fn find_pre_log(digest: &Digest) -> Result<PreLog, FindLogError> {
-	let mut found = None;
-
-	for log in digest.logs() {
-		let log = log.try_to::<PreLog>(OpaqueDigestItemId::PreRuntime(&FRONTIER_ENGINE_ID));
-		match (log, found.is_some()) {
-			(Some(_), true) => return Err(FindLogError::MultipleLogs),
-			(Some(log), false) => found = Some(log),
-			(None, _) => (),
-		}
-	}
-
-	found.ok_or(FindLogError::NotFound)
+	_find_log(digest, OpaqueDigestItemId::PreRuntime(&FRONTIER_ENGINE_ID))
 }
 
 pub fn find_post_log(digest: &Digest) -> Result<PostLog, FindLogError> {
+	_find_log(digest, OpaqueDigestItemId::Consensus(&FRONTIER_ENGINE_ID))
+}
+
+fn _find_log<Log: Decode>(
+	digest: &Digest,
+	digest_item_id: OpaqueDigestItemId,
+) -> Result<Log, FindLogError> {
 	let mut found = None;
 
 	for log in digest.logs() {
-		let log = log.try_to::<PostLog>(OpaqueDigestItemId::Consensus(&FRONTIER_ENGINE_ID));
+		let log = log.try_to::<Log>(digest_item_id);
 		match (log, found.is_some()) {
 			(Some(_), true) => return Err(FindLogError::MultipleLogs),
 			(Some(log), false) => found = Some(log),
@@ -141,27 +137,5 @@ pub fn find_log(digest: &Digest) -> Result<Log, FindLogError> {
 }
 
 pub fn ensure_log(digest: &Digest) -> Result<(), FindLogError> {
-	let mut found = false;
-
-	for log in digest.logs() {
-		let pre_log = log.try_to::<PreLog>(OpaqueDigestItemId::PreRuntime(&FRONTIER_ENGINE_ID));
-		match (pre_log, found) {
-			(Some(_), true) => return Err(FindLogError::MultipleLogs),
-			(Some(_), false) => found = true,
-			(None, _) => (),
-		}
-
-		let post_log = log.try_to::<PostLog>(OpaqueDigestItemId::Consensus(&FRONTIER_ENGINE_ID));
-		match (post_log, found) {
-			(Some(_), true) => return Err(FindLogError::MultipleLogs),
-			(Some(_), false) => found = true,
-			(None, _) => (),
-		}
-	}
-
-	if found {
-		Ok(())
-	} else {
-		Err(FindLogError::NotFound)
-	}
+	find_log(digest).map(|_log| ())
 }
