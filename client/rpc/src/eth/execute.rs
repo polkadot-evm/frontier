192e73f7e2dfa57f2b072650beb0aba66c1d694e
tgmichel <telmo@purestake.com>
2022-05-04 09:48:08 +0200

    cfg one liner

--- client/rpc/src/eth/execute.rs
+++ client/rpc/src/eth/execute.rs
@@ -561,10 +561,7 @@ where
 
 			// Verify that the transaction succeed with highest capacity
 			let cap = highest;
-			#[cfg(feature = "rpc_binary_search_estimate")]
-			let estimate_mode = false;
-			#[cfg(not(feature = "rpc_binary_search_estimate"))]
-			let estimate_mode = true;
+			let estimate_mode = !cfg!(feature = "rpc_binary_search_estimate");
 			let ExecutableResult {
 				data,
 				exit_reason,
