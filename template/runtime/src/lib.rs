cd7338e98ef9199d0196e3ebcf964369a8a394f5
bear <boundless.forest@outlook.com>
2022-05-11 20:05:20 +0800

    Fix clippy

--- template/runtime/src/lib.rs
+++ template/runtime/src/lib.rs
@@ -26,9 +26,7 @@ use sp_runtime::{
 		AccountIdLookup, BlakeTwo256, Block as BlockT, DispatchInfoOf, Dispatchable,
 		IdentifyAccount, NumberFor, PostDispatchInfoOf, Verify,
 	},
-	transaction_validity::{
-		InvalidTransaction, TransactionSource, TransactionValidity, TransactionValidityError,
-	},
+	transaction_validity::{TransactionSource, TransactionValidity, TransactionValidityError},
 	ApplyExtrinsicResult, MultiSignature,
 };
 use sp_std::{marker::PhantomData, prelude::*};
