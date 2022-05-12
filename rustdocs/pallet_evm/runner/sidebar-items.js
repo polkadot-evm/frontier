f4823cb5446a4a19207d5c020b43f9392f929f61
sorpaas <sorpaas@users.noreply.github.com>
2022-05-12 02:05:57 +0000

    deploy: 0fbf7341475ecd875f9f180d7ffa37c17c35138f

--- rustdocs/pallet_evm/runner/sidebar-items.js
+++ rustdocs/pallet_evm/runner/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["stack","EVM stack-based runner."]],"trait":[["Runner",""]]});
+initSidebarItems({"mod":[["stack","EVM stack-based runner."]],"struct":[["RunnerError",""]],"trait":[["Runner",""]]});
