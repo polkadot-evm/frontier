4d978b1251bdac2cac4e0a2a04f968f945927e32
sorpaas <sorpaas@users.noreply.github.com>
2022-05-12 00:48:16 +0000

    deploy: 4ad1dabd02b0998f7ad03da96575da994ce3b50b

--- rustdocs/implementors/pallet_evm/trait.GasWeightMapping.js
+++ rustdocs/implementors/pallet_evm/trait.GasWeightMapping.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
+implementors["frontier_template_runtime"] = [{"text":"impl <a class=\"trait\" href=\"pallet_evm/trait.GasWeightMapping.html\" title=\"trait pallet_evm::GasWeightMapping\">GasWeightMapping</a> for <a class=\"struct\" href=\"frontier_template_runtime/struct.FixedGasWeightMapping.html\" title=\"struct frontier_template_runtime::FixedGasWeightMapping\">FixedGasWeightMapping</a>","synthetic":false,"types":["frontier_template_runtime::FixedGasWeightMapping"]}];
 implementors["pallet_evm"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
