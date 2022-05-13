6463299d410fe1ccaa91c35a6c38e35fd74a2c2b
tgmichel <telmo@purestake.com>
2022-05-13 13:04:36 +0200

    wip validation primitive

--- primitives/evm/src/lib.rs
+++ primitives/evm/src/lib.rs
@@ -24,7 +24,7 @@ pub use evm::ExitReason;
 use frame_support::weights::Weight;
 #[cfg(feature = "std")]
 use serde::{Deserialize, Serialize};
-use sp_core::{H160, U256};
+use sp_core::{H160, H256, U256};
 use sp_std::vec::Vec;
 
 pub use evm::backend::{Basic as Account, Log};
@@ -86,5 +86,136 @@ pub trait FeeCalculator {
 impl FeeCalculator for () {
 	fn min_gas_price() -> (U256, Weight) {
 		(U256::zero(), 0u64)
+	}
+}
+
+pub struct CheckEvmTransactionInput {
+	pub chain_id: Option<u64>,
+	pub to: Option<H160>,
+	pub input: Vec<u8>,
+	pub nonce: U256,
+	pub gas_limit: U256,
+	pub gas_price: Option<U256>,
+	pub max_fee_per_gas: Option<U256>,
+	pub max_priority_fee_per_gas: Option<U256>,
+	pub value: U256,
+	pub access_list: Vec<(H160, Vec<H256>)>,
+}
+
+pub struct CheckEvmTransactionConfig<'config> {
+	pub evm_config: &'config evm::Config,
+	pub block_gas_limit: U256,
+	pub base_fee: U256,
+	pub chain_id: u64,
+}
+
+pub struct CheckEvmTransaction<'config, E: From<InvalidEvmTransactionError>> {
+	pub config: CheckEvmTransactionConfig<'config>,
+	pub transaction: CheckEvmTransactionInput,
+	pub _marker: sp_std::marker::PhantomData<E>,
+}
+
+type EvmTransactionValidity<E> = Result<(), E>;
+
+pub enum InvalidEvmTransactionError {
+	GasLimitTooLow,
+	GasLimitTooHigh,
+	GasPriceTooLow,
+	PriorityFeeTooHigh,
+	BalanceTooLow,
+	TxNonceTooLow,
+	TxNonceTooHigh,
+	InvalidPaymentInput,
+	InvalidChainId,
+}
+
+impl<'config, E: From<InvalidEvmTransactionError>> CheckEvmTransaction<'config, E> {
+	pub fn validate_for(&self, who: &Account) -> EvmTransactionValidity<E> {
+		if self.transaction.nonce < who.nonce {
+			return Err(InvalidEvmTransactionError::TxNonceTooLow.into());
+		}
+		self.validate(who)
+	}
+
+	pub fn validate_in_block_for(&self, who: &Account) -> EvmTransactionValidity<E> {
+		if self.transaction.nonce > who.nonce {
+			return Err(InvalidEvmTransactionError::TxNonceTooHigh.into());
+		} else if self.transaction.nonce < who.nonce {
+			return Err(InvalidEvmTransactionError::TxNonceTooLow.into());
+		}
+		self.validate(who)
+	}
+
+	pub fn with_chain_id(&self) -> Result<&Self, E>  {
+		if let Some(chain_id) = self.transaction.chain_id {
+			if chain_id != self.config.chain_id {
+				return Err(InvalidEvmTransactionError::InvalidChainId.into());
+			}
+		}
+		Ok(self)
+	}
+
+	fn validate(&self, who: &Account) -> EvmTransactionValidity<E> {
+		// We must ensure a transaction can pay the cost of its data bytes.
+		// If it can't it should not be included in a block.
+		let mut gasometer = evm::gasometer::Gasometer::new(
+			self.transaction.gas_limit.low_u64(),
+			self.config.evm_config,
+		);
+		let transaction_cost = if self.transaction.to.is_some() {
+			evm::gasometer::call_transaction_cost(
+				&self.transaction.input,
+				&self.transaction.access_list,
+			)
+		} else {
+			evm::gasometer::create_transaction_cost(
+				&self.transaction.input,
+				&self.transaction.access_list,
+			)
+		};
+		if gasometer.record_transaction(transaction_cost).is_err() {
+			return Err(InvalidEvmTransactionError::GasLimitTooLow.into());
+		}
+
+		// Transaction gas limit is within the upper bound block gas limit. 
+		if self.transaction.gas_limit >= self.config.block_gas_limit {
+			return Err(InvalidEvmTransactionError::GasLimitTooHigh.into());
+		}
+
+		// Get fee data from either a legacy or typed transaction input.
+		let max_fee_per_gas = match (
+			self.transaction.gas_price,
+			self.transaction.max_fee_per_gas,
+			self.transaction.max_priority_fee_per_gas,
+		) {
+			// Legacy or EIP-2930 transaction.
+			(Some(gas_price), None, None) => {
+				gas_price
+			}
+			// EIP-1559 transaction without tip.
+			(None, Some(max_fee_per_gas), None) => max_fee_per_gas,
+			// EIP-1559 transaction with tip.
+			(None, Some(max_fee_per_gas), Some(max_priority_fee_per_gas)) => {
+				if max_priority_fee_per_gas > max_fee_per_gas {
+					return Err(InvalidEvmTransactionError::PriorityFeeTooHigh.into());
+				}
+				max_fee_per_gas
+			}
+			_ => return Err(InvalidEvmTransactionError::InvalidPaymentInput.into()),
+		};
+
+		// Transaction max fee is at least the current base fee. 
+		if max_fee_per_gas < self.config.base_fee {
+			return Err(InvalidEvmTransactionError::GasPriceTooLow.into())
+		}
+
+		// Account has enough funds to pay for the transaction.
+		let fee = max_fee_per_gas.saturating_mul(self.transaction.gas_limit);
+		let total_payment = self.transaction.value.saturating_add(fee);
+		if who.balance < total_payment {
+			return Err(InvalidEvmTransactionError::BalanceTooLow.into());
+		}
+
+		Ok(())
 	}
 }
