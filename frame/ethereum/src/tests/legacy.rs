329cb5e97d3dd3c157e3ab892ca522a1302ca7db
tgmichel <telmo@purestake.com>
2022-06-15 10:27:23 +0200

    Add test xcm contract call works
    (cherry picked from commit f3d73cd9dee70f3a3e82987720adf2f780a5e09f)

--- frame/ethereum/src/tests/legacy.rs
+++ frame/ethereum/src/tests/legacy.rs
@@ -27,6 +27,17 @@ use frame_support::{
 };
 use sp_runtime::{DispatchError, DispatchErrorWithPostInfo};
 
+// 	pragma solidity ^0.6.6;
+// 	contract Test {
+// 		function foo() external pure returns (bool) {
+// 			return true;
+// 		}
+// 		function bar() external pure {
+// 			require(false, "error_msg");
+// 		}
+// 	}
+const CONTRACT: &str = "608060405234801561001057600080fd5b50610113806100206000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c8063c2985578146037578063febb0f7e146057575b600080fd5b603d605f565b604051808215151515815260200191505060405180910390f35b605d6068565b005b60006001905090565b600060db576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260098152602001807f6572726f725f6d7367000000000000000000000000000000000000000000000081525060200191505060405180910390fd5b56fea2646970667358221220fde68a3968e0e99b16fabf9b2997a78218b32214031f8e07e2c502daf603a69e64736f6c63430006060033";
+
 fn legacy_erc20_creation_unsigned_transaction() -> LegacyUnsignedTransaction {
 	LegacyUnsignedTransaction {
 		nonce: U256::zero(),
@@ -49,6 +60,21 @@ fn xcm_evm_transfer_legacy_transaction(destination: H160, value: U256) -> Ethere
 		action: ethereum::TransactionAction::Call(destination),
 		value,
 		input: vec![],
+		access_list: None,
+	})
+}
+
+fn xcm_evm_call_eip_legacy_transaction(destination: H160, input: Vec<u8>) -> EthereumXcmTransaction {
+	EthereumXcmTransaction::V1(EthereumXcmTransactionV1 {
+		fee_payment: EthereumXcmFee::Manual(ManualEthereumXcmFee {
+			gas_price: Some(U256::from(1)),
+			max_fee_per_gas: None,
+			max_priority_fee_per_gas: None,
+		}),
+		gas_limit: U256::from(0x100000),
+		action: ethereum::TransactionAction::Call(destination),
+		value: U256::zero(),
+		input,
 		access_list: None,
 	})
 }
@@ -301,17 +327,6 @@ fn transaction_should_generate_correct_gas_used() {
 
 #[test]
 fn call_should_handle_errors() {
-	// 	pragma solidity ^0.6.6;
-	// 	contract Test {
-	// 		function foo() external pure returns (bool) {
-	// 			return true;
-	// 		}
-	// 		function bar() external pure {
-	// 			require(false, "error_msg");
-	// 		}
-	// 	}
-	let contract: &str = "608060405234801561001057600080fd5b50610113806100206000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c8063c2985578146037578063febb0f7e146057575b600080fd5b603d605f565b604051808215151515815260200191505060405180910390f35b605d6068565b005b60006001905090565b600060db576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260098152602001807f6572726f725f6d7367000000000000000000000000000000000000000000000081525060200191505060405180910390fd5b56fea2646970667358221220fde68a3968e0e99b16fabf9b2997a78218b32214031f8e07e2c502daf603a69e64736f6c63430006060033";
-
 	let (pairs, mut ext) = new_test_ext(1);
 	let alice = &pairs[0];
 
@@ -322,7 +337,7 @@ fn call_should_handle_errors() {
 			gas_limit: U256::from(0x100000),
 			action: ethereum::TransactionAction::Create,
 			value: U256::zero(),
-			input: hex::decode(contract).unwrap(),
+			input: hex::decode(CONTRACT).unwrap(),
 		}
 		.sign(&alice.private_key);
 		assert_ok!(Ethereum::execute(alice.address, &t, None,));
@@ -409,5 +424,63 @@ fn test_transact_xcm_create() {
 				error: DispatchError::Other("Cannot convert xcm payload to known type"),
 			}
 		);
+	});
+}
+
+#[test]
+fn test_transact_xcm_evm_call_works() {
+	let (pairs, mut ext) = new_test_ext(2);
+	let alice = &pairs[0];
+	let bob = &pairs[1];
+
+	ext.execute_with(|| {
+		let t = LegacyUnsignedTransaction {
+			nonce: U256::zero(),
+			gas_price: U256::from(1),
+			gas_limit: U256::from(0x100000),
+			action: ethereum::TransactionAction::Create,
+			value: U256::zero(),
+			input: hex::decode(CONTRACT).unwrap(),
+		}
+		.sign(&alice.private_key);
+		assert_ok!(Ethereum::execute(alice.address, &t, None,));
+
+		let contract_address = hex::decode("32dcab0ef3fb2de2fce1d2e0799d36239671f04a").unwrap();
+		let foo = hex::decode("c2985578").unwrap();
+		let bar = hex::decode("febb0f7e").unwrap();
+
+		let _ = Ethereum::transact_xcm(
+			RawOrigin::XcmEthereumTransaction(bob.address).into(),
+			xcm_evm_call_eip_legacy_transaction(H160::from_slice(&contract_address), foo),
+		).expect("Failed to call `foo`");
+
+		// Evm call failing still succesfully dispatched
+		let _ = Ethereum::transact_xcm(
+			RawOrigin::XcmEthereumTransaction(bob.address).into(),
+			xcm_evm_call_eip_legacy_transaction(H160::from_slice(&contract_address), bar),
+		).expect("Failed to call `bar`");
+
+		let pending = crate::Pending::<Test>::get();
+		assert!(pending.len() == 2);
+
+		// Transaction is in Pending storage, with nonce 0 and status 1 (evm succeed).
+		let (transaction_0, _, receipt_0) = &pending[0];
+		match (transaction_0, receipt_0) {
+			(&crate::Transaction::Legacy(ref t), &crate::Receipt::Legacy(ref r)) => {
+				assert!(t.nonce == U256::from(0u8));
+				assert!(r.status_code == 1u8);
+			},
+			_ => unreachable!(),
+		}
+
+		// Transaction is in Pending storage, with nonce 1 and status 0 (evm failed).
+		let (transaction_1, _, receipt_1) = &pending[1];
+		match (transaction_1, receipt_1) {
+			(&crate::Transaction::Legacy(ref t), &crate::Receipt::Legacy(ref r)) => {
+				assert!(t.nonce == U256::from(1u8));
+				assert!(r.status_code == 0u8);
+			},
+			_ => unreachable!(),
+		}
 	});
 }
