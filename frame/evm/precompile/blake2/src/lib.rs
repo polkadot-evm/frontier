32068e2f939d2f7265dc561cb04a8fca65bc7856
nanocryk <6422796+nanocryk@users.noreply.github.com>
2022-06-23 09:32:51 +0000

    fmt

--- frame/evm/precompile/blake2/src/lib.rs
+++ frame/evm/precompile/blake2/src/lib.rs
@@ -54,7 +54,7 @@ impl Precompile for Blake2F {
 
 		let gas_cost: u64 = (rounds as u64) * Blake2F::GAS_COST_PER_ROUND;
 		handle.record_cost(gas_cost)?;
-		
+
 		let input = handle.input();
 
 		// we use from_le_bytes below to effectively swap byte order to LE if architecture is BE
