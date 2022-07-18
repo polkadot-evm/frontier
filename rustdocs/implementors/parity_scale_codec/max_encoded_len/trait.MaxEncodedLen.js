115a3b36853820fcb8b40159187f3d288c0e0de8
sorpaas <sorpaas@users.noreply.github.com>
2022-07-18 09:33:51 +0000

    deploy: cb041f1eb4f4acb7f9539d13d0b3e30a78b185a5

---
+++ rustdocs/implementors/parity_scale_codec/max_encoded_len/trait.MaxEncodedLen.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["frontier_template_runtime"] = [{"text":"impl MaxEncodedLen for <a class=\"enum\" href=\"frontier_template_runtime/enum.OriginCaller.html\" title=\"enum frontier_template_runtime::OriginCaller\">OriginCaller</a>","synthetic":false,"types":["frontier_template_runtime::OriginCaller"]}];
+implementors["pallet_ethereum"] = [{"text":"impl MaxEncodedLen for <a class=\"enum\" href=\"pallet_ethereum/enum.RawOrigin.html\" title=\"enum pallet_ethereum::RawOrigin\">RawOrigin</a>","synthetic":false,"types":["pallet_ethereum::RawOrigin"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
