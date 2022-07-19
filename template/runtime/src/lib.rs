ff6d34acdc1d1928d197d7cd1ceebaa06c61acb7
koushiro <koushiro.cqx@gmail.com>
2022-07-19 11:44:21 +0800

    Release polkadot-v0.9.25
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- template/runtime/src/lib.rs
+++ template/runtime/src/lib.rs
@@ -284,7 +284,6 @@ parameter_types! {
 }
 
 impl pallet_transaction_payment::Config for Runtime {
-	type Event = Event;
 	type OnChargeTransaction = CurrencyAdapter<Balances, ()>;
 	type OperationalFeeMultiplier = ConstU8<5>;
 	type WeightToFee = IdentityFee<Balance>;
