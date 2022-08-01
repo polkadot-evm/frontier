6d26063d2a2ac1a65944255d9312132747e41756
nanocryk <6422796+nanocryk@users.noreply.github.com>
2022-08-01 15:17:16 +0000

    prettier

--- ts-tests/tests/test-execute.ts
+++ ts-tests/tests/test-execute.ts
@@ -9,70 +9,81 @@ const EXTRINSIC_GAS_LIMIT = 64995685;
 
 describeWithFrontier("Frontier RPC (RPC execution)", (context) => {
 	step("should call with gas limit under block gas limit", async function () {
-		const result = await customRequest(context.web3, "eth_call", [{
-			from: GENESIS_ACCOUNT,
-			to: GENESIS_ACCOUNT,
-			value: "0x00",
-			gas: `0x${BLOCK_GAS_LIMIT.toString(16)}`,
-		}]);
+		const result = await customRequest(context.web3, "eth_call", [
+			{
+				from: GENESIS_ACCOUNT,
+				to: GENESIS_ACCOUNT,
+				value: "0x00",
+				gas: `0x${BLOCK_GAS_LIMIT.toString(16)}`,
+			},
+		]);
 
 		expect(result.result).to.be.equal("0x");
 	});
 
 	step("should call with gas limit up to 10x block gas limit", async function () {
-		const result = await customRequest(context.web3, "eth_call", [{
-			from: GENESIS_ACCOUNT,
-			to: GENESIS_ACCOUNT,
-			value: "0x00",
-			gas: `0x${(BLOCK_GAS_LIMIT * 10).toString(16)}`,
-		}]);
+		const result = await customRequest(context.web3, "eth_call", [
+			{
+				from: GENESIS_ACCOUNT,
+				to: GENESIS_ACCOUNT,
+				value: "0x00",
+				gas: `0x${(BLOCK_GAS_LIMIT * 10).toString(16)}`,
+			},
+		]);
 
 		expect(result.result).to.be.equal("0x");
 	});
 
 	step("shouldn't call with gas limit up higher than 10x block gas limit", async function () {
-		const result = await customRequest(context.web3, "eth_call", [{
-			from: GENESIS_ACCOUNT,
-			to: GENESIS_ACCOUNT,
-			value: "0x00",
-			gas: `0x${(BLOCK_GAS_LIMIT * 10 + 1).toString(16)}`,
-		}]);
+		const result = await customRequest(context.web3, "eth_call", [
+			{
+				from: GENESIS_ACCOUNT,
+				to: GENESIS_ACCOUNT,
+				value: "0x00",
+				gas: `0x${(BLOCK_GAS_LIMIT * 10 + 1).toString(16)}`,
+			},
+		]);
 
-		
 		expect((result as any).error.message).to.be.equal(
 			"provided gas limit is too high (can be up to 10x the block gas limit)"
 		);
 	});
 
 	step("should estimateGas with gas limit under block gas limit", async function () {
-		const result = await customRequest(context.web3, "eth_estimateGas", [{
-			from: GENESIS_ACCOUNT,
-			to: GENESIS_ACCOUNT,
-			value: "0x00",
-			gas: `0x${BLOCK_GAS_LIMIT.toString(16)}`,
-		}]);
+		const result = await customRequest(context.web3, "eth_estimateGas", [
+			{
+				from: GENESIS_ACCOUNT,
+				to: GENESIS_ACCOUNT,
+				value: "0x00",
+				gas: `0x${BLOCK_GAS_LIMIT.toString(16)}`,
+			},
+		]);
 
 		expect(result.result).to.be.equal("0x5208");
 	});
 
 	step("should estimateGas with gas limit up to 10x block gas limit", async function () {
-		const result = await customRequest(context.web3, "eth_estimateGas", [{
-			from: GENESIS_ACCOUNT,
-			to: GENESIS_ACCOUNT,
-			value: "0x00",
-			gas: `0x${(BLOCK_GAS_LIMIT * 10).toString(16)}`,
-		}]);
+		const result = await customRequest(context.web3, "eth_estimateGas", [
+			{
+				from: GENESIS_ACCOUNT,
+				to: GENESIS_ACCOUNT,
+				value: "0x00",
+				gas: `0x${(BLOCK_GAS_LIMIT * 10).toString(16)}`,
+			},
+		]);
 
 		expect(result.result).to.be.equal("0x5208");
 	});
 
 	step("shouldn't estimateGas with gas limit up higher than 10x block gas limit", async function () {
-		const result = await customRequest(context.web3, "eth_estimateGas", [{
-			from: GENESIS_ACCOUNT,
-			to: GENESIS_ACCOUNT,
-			value: "0x00",
-			gas: `0x${(BLOCK_GAS_LIMIT * 10 + 1).toString(16)}`,
-		}]);
+		const result = await customRequest(context.web3, "eth_estimateGas", [
+			{
+				from: GENESIS_ACCOUNT,
+				to: GENESIS_ACCOUNT,
+				value: "0x00",
+				gas: `0x${(BLOCK_GAS_LIMIT * 10 + 1).toString(16)}`,
+			},
+		]);
 
 		console.log(result);
 		expect((result as any).error.message).to.be.equal(
