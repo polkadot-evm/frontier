560ab3d0a2f83853a02a35ededd90b1039f9f6ce
koushiro <koushiro.cqx@gmail.com>
2022-08-27 03:14:12 +0800

    Fix
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- .maintain/release-polkadot-branch.sh
+++ .maintain/release-polkadot-branch.sh
@@ -7,9 +7,9 @@ if  [[ $# -eq 1 && "${1}" == "--help" ]]; then
     echo "  ${0} [<from>] <to>, like master or polkadot-v0.9.27"
 elif [[ $# -ne 2 ]]; then
     to_branch=${1} 
-    rg "substrate" -t toml -T lock -l | xargs sed -i "s/master/$to_branch/g"
+    rg "https://github.com/paritytech/substrate" -t toml -T lock -l | xargs sed -i "s/master/$to_branch/g"
 else
     from_branch=${1}
     to_branch=${2}
-    rg "https//github.com/paritytech/substrate" -t toml -T lock -l | xargs sed -i "s/$from_branch/$to_branch/g"
+    rg "https://github.com/paritytech/substrate" -t toml -T lock -l | xargs sed -i "s/$from_branch/$to_branch/g"
 fi
