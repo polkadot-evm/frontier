4afc9d413c7d9dd8a7045d48e72691f38ab769dc
girazoki <gorka.irazoki@gmail.com>
2023-01-20 19:43:41 +0100

    Add EGA for all impl blocks taking Eth struct (#904)
    * Add EGA for all impl blocks taking Eth struct
    
    * fix typo
    
    * fix template
    
    * better type inference by setting EGA with a dedicated function
    
    * remove anotations
    
    Co-authored-by: nanocryk <6422796+nanocryk@users.noreply.github.com>

--- client/rpc/src/eth/mod.rs
+++ client/rpc/src/eth/mod.rs
@@ -79,7 +79,7 @@ pub struct Eth<B: BlockT, C, P, CT, BE, H: ExHashT, A: ChainApi, EGA = ()> {
 	_marker: PhantomData<(B, BE, EGA)>,
 }
 
-impl<B: BlockT, C, P, CT, BE, H: ExHashT, A: ChainApi> Eth<B, C, P, CT, BE, H, A> {
+impl<B: BlockT, C, P, CT, BE, H: ExHashT, A: ChainApi> Eth<B, C, P, CT, BE, H, A, ()> {
 	pub fn new(
 		client: Arc<C>,
 		pool: Arc<P>,
@@ -114,8 +114,48 @@ impl<B: BlockT, C, P, CT, BE, H: ExHashT, A: ChainApi> Eth<B, C, P, CT, BE, H, A
 	}
 }
 
+impl<B: BlockT, C, P, CT, BE, H: ExHashT, A: ChainApi, EGA> Eth<B, C, P, CT, BE, H, A, EGA> {
+	pub fn with_estimate_gas_adapter<EGA2: EstimateGasAdapter>(
+		self,
+	) -> Eth<B, C, P, CT, BE, H, A, EGA2> {
+		let Self {
+			client,
+			pool,
+			graph,
+			convert_transaction,
+			network,
+			is_authority,
+			signers,
+			overrides,
+			backend,
+			block_data_cache,
+			fee_history_cache,
+			fee_history_cache_limit,
+			execute_gas_limit_multiplier,
+			_marker: _,
+		} = self;
+
+		Eth {
+			client,
+			pool,
+			graph,
+			convert_transaction,
+			network,
+			is_authority,
+			signers,
+			overrides,
+			backend,
+			block_data_cache,
+			fee_history_cache,
+			fee_history_cache_limit,
+			execute_gas_limit_multiplier,
+			_marker: PhantomData,
+		}
+	}
+}
+
 #[async_trait]
-impl<B, C, P, CT, BE, H: ExHashT, A> EthApiServer for Eth<B, C, P, CT, BE, H, A>
+impl<B, C, P, CT, BE, H: ExHashT, A, EGA> EthApiServer for Eth<B, C, P, CT, BE, H, A, EGA>
 where
 	B: BlockT<Hash = H256> + Send + Sync + 'static,
 	C: ProvideRuntimeApi<B> + StorageProvider<B, BE>,
@@ -126,6 +166,7 @@ where
 	BE: Backend<B> + 'static,
 	BE::State: StateBackend<BlakeTwo256>,
 	A: ChainApi<Block = B> + 'static,
+	EGA: EstimateGasAdapter + Send + Sync + 'static,
 {
 	// ########################################################################
 	// Client
