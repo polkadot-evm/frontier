784992d1cd1a9889bae8afb14a216258edafc7aa
Nisheeth Barthwal <nbaztec@gmail.com>
2023-02-10 17:23:59 +0100

    apply code suggestions from #980 (#997)
    * apply code suggestions from #980
    
    * fix lint

--- client/rpc/src/lib.rs
+++ client/rpc/src/lib.rs
@@ -59,7 +59,7 @@ pub mod frontier_backend_client {
 	use sp_blockchain::HeaderBackend;
 	use sp_runtime::{
 		generic::BlockId,
-		traits::{BlakeTwo256, Block as BlockT, Header as HeaderT, UniqueSaturatedInto, Zero},
+		traits::{BlakeTwo256, Block as BlockT, UniqueSaturatedInto, Zero},
 	};
 	use sp_storage::StorageKey;
 	// Frontier
@@ -151,11 +151,8 @@ pub mod frontier_backend_client {
 		BE: Backend<B> + 'static,
 		BE::State: StateBackend<BlakeTwo256>,
 	{
-		let Ok(at) = client.expect_block_hash_from_id(&at) else {
-			return EthereumStorageSchema::Undefined
-		};
-		if let Ok(Some(header)) = client.header(at) {
-			match client.storage(header.hash(), &StorageKey(PALLET_ETHEREUM_SCHEMA.to_vec())) {
+		if let Ok(Some(hash)) = client.block_hash_from_id(&at) {
+			match client.storage(hash, &StorageKey(PALLET_ETHEREUM_SCHEMA.to_vec())) {
 				Ok(Some(bytes)) => Decode::decode(&mut &bytes.0[..])
 					.ok()
 					.unwrap_or(EthereumStorageSchema::Undefined),
