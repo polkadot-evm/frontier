2ecee609eb25d9bfc51e2e2e1b014ce304f92b47
bernardo <bernardo@parity.io>
2023-02-28 19:46:28 -0300

    bootstrap evmless-tests

---
+++ evmless-tests/README.md
@@ -0,0 +1,17 @@
+1. Install [mocha](https://mochajs.org/):
+```
+$ npm install --global mocha
+```
+
+2. Build the EVMless Frontier Node Template:
+```
+$ cargo build --release
+$ ./target/release/frontier-template-node --dev
+```
+
+3. Run the tests:
+```
+$ cd evmless-tests
+$ npm install
+$ mocha evmless-erc20.test.js
+```
---
+++ evmless-tests/evmless-erc20.test.js
@@ -0,0 +1,31 @@
+const { ethers } = require("ethers");
+const { solidity } = require("ethereum-waffle");
+const chai = require('chai');
+const expect = chai.expect;
+
+chai.use(solidity);
+
+describe('tests', function () {
+  const initialSupply = new ethers.BigNumber.from("1337");
+  const evmlessErc20Address = "0x0000000000000000000000000000000000000539";
+  const evmlessErc20Abi = [
+    "function name() public view returns (string)",
+    "function symbol() public view returns (string)",
+    "function decimals() public view returns (uint8)",
+    "function totalSupply() public view returns (uint256)",
+    "function balanceOf(address _owner) public view returns (uint256 balance)",
+    "function transfer(address _to, uint256 _value) public returns (bool success)",
+    "function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)",
+    "function approve(address _spender, uint256 _value) public returns (bool success)",
+    "function allowance(address _owner, address _spender) public view returns (uint256 remaining)",
+  ];
+
+  before(async function () {
+    this.Provider = new ethers.providers.JsonRpcProvider('http://localhost::9933');
+    this.evmlessErc20Contract = new ethers.Contract(evmlessErc20Address, evmlessErc20Abi, this.Provider);
+  });
+
+  it('correct initial supply', async function () {
+    expect(await this.evmlessErc20Contract.totalSupply()).to.equal(initialSupply);
+  });
+});
