5bfa9b52e6bec2466d93d80430dfc27a5595e600
bernardo <bernardo@parity.io>
2023-03-01 18:39:51 -0300

    comment out unimplemented erc20 methods from tests

--- evmless-tests/evmless-erc20.test.js
+++ evmless-tests/evmless-erc20.test.js
@@ -16,11 +16,11 @@ describe('tests', function () {
     "function symbol() public view returns (string)",
     "function decimals() public view returns (uint8)",
     "function totalSupply() public view returns (uint256)",
-    "function balanceOf(address _owner) public view returns (uint256 balance)",
-    "function transfer(address _to, uint256 _value) public returns (bool success)",
-    "function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)",
-    "function approve(address _spender, uint256 _value) public returns (bool success)",
-    "function allowance(address _owner, address _spender) public view returns (uint256 remaining)",
+    // "function balanceOf(address _owner) public view returns (uint256 balance)",
+    // "function transfer(address _to, uint256 _value) public returns (bool success)",
+    // "function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)",
+    // "function approve(address _spender, uint256 _value) public returns (bool success)",
+    // "function allowance(address _owner, address _spender) public view returns (uint256 remaining)",
   ];
 
   before(async function () {
