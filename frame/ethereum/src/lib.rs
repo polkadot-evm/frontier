9af9504776d16211c1650f59435b7df7da3f2a04
tgmichel <telmo@purestake.com>
2023-03-01 14:01:03 +0100

    catch ethereum execution infp

--- frame/ethereum/src/lib.rs
+++ frame/ethereum/src/lib.rs
@@ -27,6 +27,7 @@
 
 extern crate alloc;
 
+mod catch_exec_info;
 #[cfg(all(feature = "std", test))]
 mod mock;
 #[cfg(all(feature = "std", test))]
@@ -38,6 +39,8 @@ pub use ethereum::{
 	AccessListItem, BlockV2 as Block, LegacyTransactionMessage, Log, ReceiptV3 as Receipt,
 	TransactionAction, TransactionV2 as Transaction,
 };
+pub use catch_exec_info::catch_exec_info;
+
 use ethereum_types::{Bloom, BloomInput, H160, H256, H64, U256};
 use evm::ExitReason;
 use scale_codec::{Decode, Encode, MaxEncodedLen};
@@ -575,6 +578,8 @@ impl<T: Config> Pallet<T> {
 		transaction: Transaction,
 	) -> Result<(PostDispatchInfo, CallOrCreateInfo), DispatchErrorWithPostInfo> {
 		let (to, _, info) = Self::execute(source, &transaction, None)?;
+
+		catch_exec_info::fill_exec_info(&info);
 
 		let pending = Pending::<T>::get();
 		let transaction_hash = transaction.hash();
