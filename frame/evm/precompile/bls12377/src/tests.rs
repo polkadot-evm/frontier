c3b37833d00b65588c946c647ceb9d9b1fdccf9b
echo <hujw77@gmail.com>
2023-05-05 13:24:12 +0800

    EIP-2539 (#15)
    * Update shell.nix
    
    * Read point from input
    
    * Finish `BLS12377G1Add`
    
    * Fix `BLS12377G1Add` output encode
    
    * Finish `BLS12377G1Mul`
    
    * Finish `BLS12377G1MultiExp`
    
    * Finish `BLS12377G2Add`
    
    * Finish `BLS12377G2Mul`
    
    * Draft `eip-2539` implement
    
    * Finish `BLS12377Pairing`
    
    * Draft `eip-2539`
    
    * Multiplication by the unnormalized scalar
    
    * Rename serialize to write
    
    * Test Cases
    
    * Test Cases
    
    * Rewrite read_fq
    
    * Rename
    
    * Doc and cleanup
    
    * Tidy
    
    * Tidy
    
    * Tidy
    
    * Only check point in subgroup for pairing
    
    * Fmt
    
    * Tests
    
    * Typo
    
    * Typo
    
    * Fix conv
    
    * Change err info
    
    * Fmt
    
    * EIP-2539 tests
    
    * EIP-2539 tests
    
    * Lint and test

---
+++ frame/evm/precompile/bls12377/src/tests.rs
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: Apache-2.0
+// This file is part of Frontier.
+//
+// Copyright (c) 2020-2022 Parity Technologies (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use super::*;
+use pallet_evm_test_vector_support::{
+	test_precompile_failure_test_vectors, test_precompile_test_vectors,
+};
+
+#[test]
+fn process_consensus_tests() -> Result<(), String> {
+	test_precompile_test_vectors::<Bls12377G1Add>("../testdata/bls12377G1Add.json")?;
+	test_precompile_test_vectors::<Bls12377G1Mul>("../testdata/bls12377G1Mul.json")?;
+	test_precompile_test_vectors::<Bls12377G1MultiExp>("../testdata/bls12377G1MultiExp.json")?;
+	test_precompile_test_vectors::<Bls12377G2Add>("../testdata/bls12377G2Add.json")?;
+	test_precompile_test_vectors::<Bls12377G2Mul>("../testdata/bls12377G2Mul.json")?;
+	test_precompile_test_vectors::<Bls12377G2MultiExp>("../testdata/bls12377G2MultiExp.json")?;
+	test_precompile_test_vectors::<Bls12377Pairing>("../testdata/bls12377Pairing.json")?;
+	Ok(())
+}
+
+#[test]
+fn process_consensus_failure_tests() -> Result<(), String> {
+	test_precompile_failure_test_vectors::<Bls12377G1Add>("../testdata/fail-bls12377G1Add.json")?;
+	test_precompile_failure_test_vectors::<Bls12377G1Mul>("../testdata/fail-bls12377G1Mul.json")?;
+	test_precompile_failure_test_vectors::<Bls12377G1MultiExp>(
+		"../testdata/fail-bls12377G1MultiExp.json",
+	)?;
+	test_precompile_failure_test_vectors::<Bls12377G2Add>("../testdata/fail-bls12377G2Add.json")?;
+	test_precompile_failure_test_vectors::<Bls12377G2Mul>("../testdata/fail-bls12377G2Mul.json")?;
+	test_precompile_failure_test_vectors::<Bls12377G2MultiExp>(
+		"../testdata/fail-bls12377G2MultiExp.json",
+	)?;
+	test_precompile_failure_test_vectors::<Bls12377Pairing>(
+		"../testdata/fail-bls12377Pairing.json",
+	)?;
+	Ok(())
+}
