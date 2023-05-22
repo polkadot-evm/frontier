832e1f7a5e2929195efab5b1361b0962a79f3e2d
sorpaas <sorpaas@users.noreply.github.com>
2023-05-22 12:19:33 +0000

    deploy: 4b1b16846aeca5856e33dc38fa5bf12e5e64ebb5

---
+++ rustdocs/implementors/evm/executor/stack/precompile/trait.PrecompileHandle.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"pallet_evm_test_vector_support":[["impl PrecompileHandle for <a class=\"struct\" href=\"pallet_evm_test_vector_support/struct.MockHandle.html\" title=\"struct pallet_evm_test_vector_support::MockHandle\">MockHandle</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
