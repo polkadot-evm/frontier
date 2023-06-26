87022030cf120f01748384079d0aef3bab89f045
sorpaas <sorpaas@users.noreply.github.com>
2023-06-26 17:20:57 +0000

    deploy: 3b871456f7a23b17fc01b2d17d895a0bdf60b725

--- rustdocs/implementors/parity_scale_codec/max_encoded_len/trait.MaxEncodedLen.js
+++ rustdocs/implementors/parity_scale_codec/max_encoded_len/trait.MaxEncodedLen.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"fp_account":[["impl MaxEncodedLen for <a class=\"struct\" href=\"fp_account/struct.AccountId20.html\" title=\"struct fp_account::AccountId20\">AccountId20</a>"]],
+"fp_account":[["impl MaxEncodedLen for <a class=\"struct\" href=\"fp_account/struct.AccountId20.html\" title=\"struct fp_account::AccountId20\">AccountId20</a>"],["impl MaxEncodedLen for <a class=\"struct\" href=\"fp_account/struct.EthereumSigner.html\" title=\"struct fp_account::EthereumSigner\">EthereumSigner</a>"]],
 "frontier_template_runtime":[["impl MaxEncodedLen for <a class=\"enum\" href=\"frontier_template_runtime/enum.OriginCaller.html\" title=\"enum frontier_template_runtime::OriginCaller\">OriginCaller</a>"]],
 "pallet_ethereum":[["impl MaxEncodedLen for <a class=\"enum\" href=\"pallet_ethereum/enum.RawOrigin.html\" title=\"enum pallet_ethereum::RawOrigin\">RawOrigin</a>"]],
 "pallet_evm":[["impl MaxEncodedLen for <a class=\"struct\" href=\"pallet_evm/struct.CodeMetadata.html\" title=\"struct pallet_evm::CodeMetadata\">CodeMetadata</a>"]]
