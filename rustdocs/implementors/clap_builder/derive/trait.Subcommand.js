df67a5e1367caee2bdd8cb997689d6d465bbe1ff
sorpaas <sorpaas@users.noreply.github.com>
2023-06-28 11:16:11 +0000

    deploy: e89f33688e0e438d0d26ca4f5eaba16a17201a22

---
+++ rustdocs/implementors/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"frontier_template_node":[["impl Subcommand for <a class=\"enum\" href=\"frontier_template_node/cli/enum.Subcommand.html\" title=\"enum frontier_template_node::cli::Subcommand\">Subcommand</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
