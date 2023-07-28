070385c8fa85f261cdb068ebdc13a9322f3f77d4
sorpaas <sorpaas@users.noreply.github.com>
2023-07-28 11:25:29 +0000

    deploy: aab7cfc3e038ec65ca4d1b2bc3d68e51aa162514

--- rustdocs/implementors/sp_runtime/trait.BuildStorage.js
+++ rustdocs/implementors/sp_runtime/trait.BuildStorage.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "frontier_template_node":[["impl BuildStorage for <a class=\"struct\" href=\"frontier_template_node/chain_spec/struct.DevGenesisExt.html\" title=\"struct frontier_template_node::chain_spec::DevGenesisExt\">DevGenesisExt</a>"]],
-"frontier_template_runtime":[["impl BuildStorage for <a class=\"struct\" href=\"frontier_template_runtime/struct.GenesisConfig.html\" title=\"struct frontier_template_runtime::GenesisConfig\">GenesisConfig</a>"]]
+"frontier_template_runtime":[["impl BuildStorage for <a class=\"struct\" href=\"frontier_template_runtime/struct.RuntimeGenesisConfig.html\" title=\"struct frontier_template_runtime::RuntimeGenesisConfig\">RuntimeGenesisConfig</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
