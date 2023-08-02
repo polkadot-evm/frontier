b168105be6e828a3012a9aa9010254f0884b5c43
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2023-08-02 14:13:34 +0300

    add unit tests and a new MetricError enum

--- primitives/evm/src/metric.rs
+++ primitives/evm/src/metric.rs
@@ -1,9 +1,17 @@
-use evm::{gasometer::GasCost, ExitError, Opcode};
+use evm::{gasometer::GasCost, Opcode};
 use sp_runtime::{
 	traits::{CheckedAdd, Zero},
 	Saturating,
 };
 
+#[derive(Debug, PartialEq)]
+/// Metric error.
+enum MetricError {
+	/// The metric usage exceeds the limit.
+	LimitExceeded,
+}
+
+/// A struct that keeps track of metric usage and limit.
 pub struct Metric<T> {
 	limit: T,
 	usage: T,
@@ -13,6 +21,7 @@ impl<T> Metric<T>
 where
 	T: Zero,
 {
+	/// Creates a new `Metric` instance with the given limit.
 	pub fn new(limit: T) -> Self {
 		Self {
 			limit,
@@ -25,41 +34,111 @@ impl<T> Metric<T>
 where
 	T: CheckedAdd + Saturating + PartialOrd + Copy,
 {
-	fn try_consume(&mut self, cost: T) -> Result<(), ExitError> {
-		let usage = self.usage.checked_add(&cost).ok_or(ExitError::OutOfGas)?;
+	/// Records the cost of an operation and updates the usage.
+	///
+	/// # Errors
+	///
+	/// Returns `MetricError::LimitExceeded` if the metric usage exceeds the limit.
+	fn record_cost(&mut self, cost: T) -> Result<(), MetricError> {
+		let usage = self
+			.usage
+			.checked_add(&cost)
+			.ok_or(MetricError::LimitExceeded)?;
 		if usage > self.limit {
-			return Err(ExitError::OutOfGas);
+			return Err(MetricError::LimitExceeded);
 		}
 		self.usage = usage;
 		Ok(())
 	}
 
+	/// Refunds the given amount.
 	fn refund(&mut self, amount: T) {
 		self.usage = self.usage.saturating_sub(amount);
 	}
-
-	fn record(&mut self, opcode: Opcode, gas_cost: GasCost) -> Result<(), ExitError> {
-		Ok(())
-	}
 }
 
+/// A struct that keeps track of new storage usage and limit.
 pub struct StorageMetric(Metric<u64>);
 
 impl StorageMetric {
+	/// Creates a new `StorageMetric` instance with the given limit.
+	pub fn new(limit: u64) -> Self {
+		Self(Metric::new(limit))
+	}
+
+	/// Refunds the given amount of storage gas.
 	fn refund(&mut self, amount: u64) {
 		self.0.refund(amount)
 	}
 
+	/// Records the dynamic opcode cost and updates the storage usage.
+	///
+	/// # Errors
+	///
+	/// Returns `MetricError::LimitExceeded` if the storage gas usage exceeds the storage gas limit.
 	fn record_dynamic_opcode_cost(
 		&mut self,
 		_opcode: Opcode,
 		gas_cost: GasCost,
-	) -> Result<(), ExitError> {
+	) -> Result<(), MetricError> {
 		let cost = match gas_cost {
 			GasCost::Create => 0,
-			GasCost::Create2 { len } => len.try_into().map_err(|_| ExitError::OutOfGas)?,
+			GasCost::Create2 { len } => len.try_into().map_err(|_| MetricError::LimitExceeded)?,
 			_ => return Ok(()),
 		};
-		self.0.try_consume(cost)
+		self.0.record_cost(cost)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_init() {
+		let metric = Metric::<u64>::new(100);
+		assert_eq!(metric.limit, 100);
+		assert_eq!(metric.usage, 0);
+	}
+
+	#[test]
+	fn test_try_consume() {
+		let mut metric = Metric::<u64>::new(100);
+		assert_eq!(metric.record_cost(10), Ok(()));
+		assert_eq!(metric.usage, 10);
+		assert_eq!(metric.record_cost(90), Ok(()));
+		assert_eq!(metric.usage, 100);
+		assert_eq!(metric.record_cost(1), Err(MetricError::LimitExceeded));
+		assert_eq!(metric.usage, 100);
+	}
+
+	#[test]
+	fn test_refund() {
+		let mut metric = Metric::<u64>::new(100);
+		assert_eq!(metric.record_cost(10), Ok(()));
+		assert_eq!(metric.usage, 10);
+		metric.refund(10);
+		assert_eq!(metric.usage, 0);
+
+		// refund more than usage
+		metric.refund(10);
+		assert_eq!(metric.usage, 0);
+	}
+
+	#[test]
+	fn test_storage_metric() {
+		let mut metric = StorageMetric::new(100);
+		assert_eq!(metric.0.usage, 0);
+		assert_eq!(metric.0.limit, 100);
+		assert_eq!(metric.0.record_cost(10), Ok(()));
+		assert_eq!(metric.0.usage, 10);
+		assert_eq!(metric.0.record_cost(90), Ok(()));
+		assert_eq!(metric.0.usage, 100);
+		assert_eq!(metric.0.record_cost(1), Err(MetricError::LimitExceeded));
+		assert_eq!(metric.0.usage, 100);
+		metric.0.refund(10);
+		assert_eq!(metric.0.usage, 90);
+		metric.refund(10);
+		assert_eq!(metric.0.usage, 80);
 	}
 }
