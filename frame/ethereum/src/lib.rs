e615550a1a9eae267cff7dbb58176dadbee7454a
librelois <c@elo.tf>
2023-08-18 17:55:24 +0200

    create helper function max_transaction_encoded_size

--- frame/ethereum/src/lib.rs
+++ frame/ethereum/src/lib.rs
@@ -361,6 +361,14 @@ pub mod pallet {
 }
 
 impl<T: Config> Pallet<T> {
+		/// The call wrapped in the extrinsic is part of the PoV, record this as a base cost for the size of the proof.
+		fn proof_size_base_cost(transaction: &Transaction) -> u64 {
+			transaction
+				.encoded_size()
+				// pallet index + call index
+				.saturating_add(2) as u64
+		}
+
 	pub fn transaction_weight(transaction_data: &TransactionData) -> (Option<Weight>, Option<u64>) {
 		match <T as pallet_evm::Config>::GasWeightMapping::gas_to_weight(
 			transaction_data.gas_limit.unique_saturated_into(),
@@ -859,6 +867,24 @@ impl<T: Config> Pallet<T> {
 				Ok((None, Some(res.value), CallOrCreateInfo::Create(res)))
 			}
 		}
+	}
+
+	/// Estimated maximal SCALE encoded size of an ethereum transaction
+	pub fn max_transaction_encoded_size(input: &[u8], access_list: &[(H160, Vec<H256>)]) -> u64 {
+		Self::proof_size_base_cost(&Transaction::EIP1559(ethereum::EIP1559Transaction{
+			chain_id: 0,
+			nonce: Default::default(),
+			max_priority_fee_per_gas: Default::default(),
+			max_fee_per_gas: Default::default(),
+			gas_limit: Default::default(),
+			action: ethereum::TransactionAction::Call(Default::default()),
+			value: Default::default(),
+			input: input.to_vec(),
+			access_list: access_list.iter().map(|(address, storage_keys)| AccessListItem { address: *address, storage_keys: storage_keys.to_vec() }).collect(),
+			odd_y_parity: false,
+			r: Default::default(),
+			s: Default::default(),
+		}))
 	}
 
 	/// Validate an Ethereum transaction already in block
