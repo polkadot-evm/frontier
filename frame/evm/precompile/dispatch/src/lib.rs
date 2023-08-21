dee68d8cf796cabb35bde6d6c75e5749245c230b
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2023-08-21 16:27:04 +0200

    fix formatting

--- frame/evm/precompile/dispatch/src/lib.rs
+++ frame/evm/precompile/dispatch/src/lib.rs
@@ -81,8 +81,11 @@ where
 			return Err(err);
 		}
 
-		handle
-			.record_external_cost(Some(info.weight.ref_time()), Some(info.weight.proof_size()), None)?;
+		handle.record_external_cost(
+			Some(info.weight.ref_time()),
+			Some(info.weight.proof_size()),
+			None,
+		)?;
 
 		match call.dispatch(Some(origin).into()) {
 			Ok(post_info) => {
