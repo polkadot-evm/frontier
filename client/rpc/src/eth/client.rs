f875b559b724945ef1313b28c0e20f61fc870a0a
yjhmelody <yjh465402634@gmail.com>
2023-08-28 17:45:10 +0800

    impl

--- client/rpc/src/eth/client.rs
+++ client/rpc/src/eth/client.rs
@@ -48,14 +48,16 @@ where
 
 	pub async fn syncing(&self) -> RpcResult<SyncStatus> {
 		if self.sync.is_major_syncing() {
-			let current_block = U256::from(UniqueSaturatedInto::<u128>::unique_saturated_into(
-				self.client.info().best_number,
-			));
-			let highest_block = self
+			let current_number = self.client.info().best_number;
+			let current_block = U256::from(UniqueSaturatedInto::<u128>::unique_saturated_into(current_number));
+			let highest_number = self
 				.sync
 				.best_seen_block()
-				.await?
-				.unwrap_or_else(|| current_block);
+				.await
+				.map_err(|_err| internal_err("fetching best_seen_block failed"))?
+				.unwrap_or_else(|| current_number);
+			let highest_block = U256::from(UniqueSaturatedInto::<u128>::unique_saturated_into(highest_number));
+
 			Ok(SyncStatus::Info(SyncInfo {
 				starting_block: U256::zero(),
 				current_block,
