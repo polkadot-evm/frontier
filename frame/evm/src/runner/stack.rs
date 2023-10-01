f4c6bbb26875664abe9fa4389aaf325ca171f3b2
Wei Tang <wei@pacna.org>
2023-10-02 01:21:58 +0200

    Use contains_key instead of is_some
    Co-authored-by: Xiliang Chen <xlchen1291@gmail.com>

--- frame/evm/src/runner/stack.rs
+++ frame/evm/src/runner/stack.rs
@@ -791,7 +791,7 @@ where
 	}
 
 	fn storage(&self, address: H160, index: H256) -> H256 {
-		if <Suicided<T>>::get(&address).is_some() {
+		if <Suicided<T>>::contains_key(&address)) {
 			return H256::default();
 		}
 
