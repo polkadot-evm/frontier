699f055836a3c2d140ca654d7847977470ab9b64
koushiro <koushiro.cqx@gmail.com>
2023-10-20 14:10:38 +0800

    chore: improve txpool rpc and add some docs

--- client/rpc-core/src/txpool.rs
+++ client/rpc-core/src/txpool.rs
@@ -26,12 +26,40 @@ use crate::types::*;
 /// TxPool rpc interface
 #[rpc(server)]
 pub trait TxPoolApi {
+	/// The content inspection property can be queried to list the exact details of all the
+	/// transactions currently pending for inclusion in the next block(s), as well as the ones that
+	/// are being scheduled for future execution only.
+	///
+	/// The result is an object with two fields pending and queued. Each of these fields are
+	/// associative arrays, in which each entry maps an origin-address to a batch of scheduled
+	/// transactions. These batches themselves are maps associating nonces with actual transactions.
+	///
+	/// For details, see [txpool_content](https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-txpool#txpool-content)
 	#[method(name = "txpool_content")]
 	fn content(&self) -> RpcResult<TxPoolResult<TransactionMap<TxPoolTransaction>>>;
 
+	/// The inspect inspection property can be queried to list a textual summary of all the
+	/// transactions currently pending for inclusion in the next block(s), as well as the ones that
+	/// are being scheduled for future execution only. This is a method specifically tailored to
+	/// developers to quickly see the transactions in the pool and find any potential issues.
+	///
+	/// The result is an object with two fields pending and queued. Each of these fields are
+	/// associative arrays, in which each entry maps an origin-address to a batch of scheduled
+	/// transactions. These batches themselves are maps associating nonces with transactions
+	/// summary strings.
+	///
+	/// For details, see [txpool_inspect](https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-txpool#txpool-content)
 	#[method(name = "txpool_inspect")]
 	fn inspect(&self) -> RpcResult<TxPoolResult<TransactionMap<Summary>>>;
 
+	/// The status inspection property can be queried for the number of transactions currently
+	/// pending for inclusion in the next block(s), as well as the ones that are being scheduled
+	/// for future execution only.
+	///
+	/// The result is an object with two fields pending and queued, each of which is a counter
+	/// representing the number of transactions in that particular state.
+	///
+	/// For details, see [txpool_status](https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-txpool#txpool-status)
 	#[method(name = "txpool_status")]
 	fn status(&self) -> RpcResult<TxPoolResult<U256>>;
 }
