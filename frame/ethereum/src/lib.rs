7503ba96672f0db070c9b7cf8a8990d3829438aa
Valentin Fernandez <tinchofernandez8@gmail.com>
2023-12-15 12:30:43 -0300

    Make Pending type available for other pallets

--- frame/ethereum/src/lib.rs
+++ frame/ethereum/src/lib.rs
@@ -317,7 +317,7 @@ pub mod pallet {
 
 	/// Current building block's transactions and receipts.
 	#[pallet::storage]
-	pub(super) type Pending<T: Config> =
+	pub type Pending<T: Config> =
 		StorageValue<_, Vec<(Transaction, TransactionStatus, Receipt)>, ValueQuery>;
 
 	/// The current Ethereum block.
