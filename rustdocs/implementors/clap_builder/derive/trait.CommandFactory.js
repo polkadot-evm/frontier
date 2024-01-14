c92cb58de2ca316414b75e474fcb39fbac3e14a3
sorpaas <sorpaas@users.noreply.github.com>
2024-01-14 22:42:27 +0000

    deploy: e262e80f3083ea8df90bd71a827789bef810f6aa

--- rustdocs/implementors/clap_builder/derive/trait.CommandFactory.js
+++ rustdocs/implementors/clap_builder/derive/trait.CommandFactory.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "fc_cli":[["impl CommandFactory for <a class=\"struct\" href=\"fc_cli/struct.FrontierDbCmd.html\" title=\"struct fc_cli::FrontierDbCmd\">FrontierDbCmd</a>"]],
-"frontier_template_node":[["impl CommandFactory for <a class=\"struct\" href=\"frontier_template_node/cli/struct.Cli.html\" title=\"struct frontier_template_node::cli::Cli\">Cli</a>"],["impl CommandFactory for <a class=\"struct\" href=\"frontier_template_node/eth/struct.EthConfiguration.html\" title=\"struct frontier_template_node::eth::EthConfiguration\">EthConfiguration</a>"]]
+"frontier_template_node":[["impl CommandFactory for <a class=\"struct\" href=\"frontier_template_node/eth/struct.EthConfiguration.html\" title=\"struct frontier_template_node::eth::EthConfiguration\">EthConfiguration</a>"],["impl CommandFactory for <a class=\"struct\" href=\"frontier_template_node/cli/struct.Cli.html\" title=\"struct frontier_template_node::cli::Cli\">Cli</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
