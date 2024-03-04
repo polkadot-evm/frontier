1dde72f5e3b6b171e7a7491b828be31793a513dc
Qinxuan Chen <koushiro.cqx@gmail.com>
2024-03-04 16:07:00 +0800

    Merge CallRequst and TransactionRequest RPC types (#1301)

--- client/rpc-core/src/types/call_request.rs
+++ client/rpc-core/src/types/call_request.rs
@@ -18,41 +18,10 @@
 
 use std::collections::BTreeMap;
 
-use ethereum::AccessListItem;
-use ethereum_types::{H160, H256, U256};
+use ethereum_types::{H256, U256};
 use serde::Deserialize;
 
-use crate::types::{deserialize_data_or_input, Bytes};
-
-/// Call request
-#[derive(Clone, Debug, Default, Eq, PartialEq, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct CallRequest {
-	/// From
-	pub from: Option<H160>,
-	/// To
-	pub to: Option<H160>,
-	/// Gas Price
-	pub gas_price: Option<U256>,
-	/// EIP-1559 Max base fee the caller is willing to pay
-	pub max_fee_per_gas: Option<U256>,
-	/// EIP-1559 Priority fee the caller is paying to the block author
-	pub max_priority_fee_per_gas: Option<U256>,
-	/// Gas
-	pub gas: Option<U256>,
-	/// Value
-	pub value: Option<U256>,
-	/// Data
-	#[serde(deserialize_with = "deserialize_data_or_input", flatten)]
-	pub data: Option<Bytes>,
-	/// Nonce
-	pub nonce: Option<U256>,
-	/// AccessList
-	pub access_list: Option<Vec<AccessListItem>>,
-	/// EIP-2718 type
-	#[serde(rename = "type")]
-	pub transaction_type: Option<U256>,
-}
+use crate::types::Bytes;
 
 // State override
 #[derive(Clone, Debug, Default, Eq, PartialEq, Deserialize)]
@@ -71,101 +40,4 @@ pub struct CallStateOverride {
 	/// Fake key-value mapping to override individual slots in the account storage before
 	/// executing the call.
 	pub state_diff: Option<BTreeMap<H256, H256>>,
-}
-
-#[cfg(test)]
-mod tests {
-	use super::*;
-	use serde_json::json;
-
-	#[test]
-	fn test_deserialize_with_only_input() {
-		let data = json!({
-			"from": "0x60be2d1d3665660d22ff9624b7be0551ee1ac91b",
-			"to": "0x13fe2d1d3665660d22ff9624b7be0551ee1ac91b",
-			"gasPrice": "0x10",
-			"maxFeePerGas": "0x20",
-			"maxPriorityFeePerGas": "0x30",
-			"gas": "0x40",
-			"value": "0x50",
-			"input": "0x123abc",
-			"nonce": "0x60",
-			"accessList": [{"address": "0x60be2d1d3665660d22ff9624b7be0551ee1ac91b", "storageKeys": []}],
-			"type": "0x70"
-		});
-
-		let request: Result<CallRequest, _> = serde_json::from_value(data);
-		assert!(request.is_ok());
-
-		let request = request.unwrap();
-		assert_eq!(request.data, Some(Bytes::from(vec![0x12, 0x3a, 0xbc])));
-	}
-
-	#[test]
-	fn test_deserialize_with_only_data() {
-		let data = json!({
-			"from": "0x60be2d1d3665660d22ff9624b7be0551ee1ac91b",
-			"to": "0x13fe2d1d3665660d22ff9624b7be0551ee1ac91b",
-			"gasPrice": "0x10",
-			"maxFeePerGas": "0x20",
-			"maxPriorityFeePerGas": "0x30",
-			"gas": "0x40",
-			"value": "0x50",
-			"data": "0x123abc",
-			"nonce": "0x60",
-			"accessList": [{"address": "0x60be2d1d3665660d22ff9624b7be0551ee1ac91b", "storageKeys": []}],
-			"type": "0x70"
-		});
-
-		let request: Result<CallRequest, _> = serde_json::from_value(data);
-		assert!(request.is_ok());
-
-		let request = request.unwrap();
-		assert_eq!(request.data, Some(Bytes::from(vec![0x12, 0x3a, 0xbc])));
-	}
-
-	#[test]
-	fn test_deserialize_with_data_and_input_mismatch() {
-		let data = json!({
-			"from": "0x60be2d1d3665660d22ff9624b7be0551ee1ac91b",
-			"to": "0x13fe2d1d3665660d22ff9624b7be0551ee1ac91b",
-			"gasPrice": "0x10",
-			"maxFeePerGas": "0x20",
-			"maxPriorityFeePerGas": "0x30",
-			"gas": "0x40",
-			"value": "0x50",
-			"data": "0x123abc",
-			"input": "0x456def",
-			"nonce": "0x60",
-			"accessList": [{"address": "0x60be2d1d3665660d22ff9624b7be0551ee1ac91b", "storageKeys": []}],
-			"type": "0x70"
-		});
-
-		let request: Result<CallRequest, _> = serde_json::from_value(data);
-		assert!(request.is_err());
-	}
-
-	#[test]
-	fn test_deserialize_with_data_and_input_equal() {
-		let data = json!({
-			"from": "0x60be2d1d3665660d22ff9624b7be0551ee1ac91b",
-			"to": "0x13fe2d1d3665660d22ff9624b7be0551ee1ac91b",
-			"gasPrice": "0x10",
-			"maxFeePerGas": "0x20",
-			"maxPriorityFeePerGas": "0x30",
-			"gas": "0x40",
-			"value": "0x50",
-			"data": "0x123abc",
-			"input": "0x123abc",
-			"nonce": "0x60",
-			"accessList": [{"address": "0x60be2d1d3665660d22ff9624b7be0551ee1ac91b", "storageKeys": []}],
-			"type": "0x70"
-		});
-
-		let request: Result<CallRequest, _> = serde_json::from_value(data);
-		assert!(request.is_ok());
-
-		let request = request.unwrap();
-		assert_eq!(request.data, Some(Bytes::from(vec![0x12, 0x3a, 0xbc])));
-	}
 }
