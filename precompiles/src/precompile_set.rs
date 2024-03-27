103250b0b17aa5808ea18a41929912231f97fe55
Dino Pacandi <dino.pacandi@gmail.com>
2024-03-27 16:35:06 +0100

    Formating

--- precompiles/src/precompile_set.rs
+++ precompiles/src/precompile_set.rs
@@ -1089,14 +1089,13 @@ impl<R: pallet_evm::Config, P: PrecompileSetFragment> PrecompileSetBuilder<R, P>
 
 	/// Return the list of mapped addresses contained in this PrecompileSet.
 	pub fn used_addresses() -> impl Iterator<Item = R::AccountId> {
-		Self::used_addresses_h160()
-			.map(R::AddressMapping::into_account_id)
+		Self::used_addresses_h160().map(R::AddressMapping::into_account_id)
 	}
 
 	/// Return the list of H160 addresses contained in this PrecompileSet.
 	pub fn used_addresses_h160() -> impl Iterator<Item = H160> {
-        Self::new().inner.used_addresses().into_iter()
-    }
+		Self::new().inner.used_addresses().into_iter()
+	}
 
 	pub fn summarize_checks(&self) -> Vec<PrecompileCheckSummary> {
 		self.inner.summarize_checks()
