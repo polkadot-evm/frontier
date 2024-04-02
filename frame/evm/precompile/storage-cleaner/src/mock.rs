1c9f297c2787db897361b70190a0080c65025a17
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2024-04-02 13:42:58 +0300

    Fix clippy warnings

--- frame/evm/precompile/storage-cleaner/src/mock.rs
+++ frame/evm/precompile/storage-cleaner/src/mock.rs
@@ -70,6 +70,11 @@ impl frame_system::Config for Runtime {
 	type SS58Prefix = ();
 	type OnSetCode = ();
 	type MaxConsumers = ConstU32<16>;
+	type MultiBlockMigrator = ();
+	type PreInherents = ();
+	type PostInherents = ();
+	type PostTransactions = ();
+	type SingleBlockMigrations = ();
 }
 
 parameter_types! {
