f1e1694ac5cfbad0a773aa2d36318e81dcbd62ca
magecnion <magecnion@gmail.com>
2024-12-18 12:59:48 +0100

    run taplo

--- test-utils/runtime/src/lib.rs
+++ test-utils/runtime/src/lib.rs
@@ -95,8 +95,7 @@ pub mod wasm_binary_logging_disabled {
 #[cfg(feature = "std")]
 pub fn wasm_binary_unwrap() -> &'static [u8] {
 	WASM_BINARY.expect(
-		"Development wasm binary is not available. Testing is only supported with the flag
-		 disabled.",
+		"Development wasm binary is not available. Testing is only supported with the flag disabled.",
 	)
 }
 
@@ -104,8 +103,7 @@ pub fn wasm_binary_unwrap() -> &'static [u8] {
 #[cfg(feature = "std")]
 pub fn wasm_binary_logging_disabled_unwrap() -> &'static [u8] {
 	wasm_binary_logging_disabled::WASM_BINARY.expect(
-		"Development wasm binary is not available. Testing is only supported with the flag
-		 disabled.",
+		"Development wasm binary is not available. Testing is only supported with the flag disabled.",
 	)
 }
 
