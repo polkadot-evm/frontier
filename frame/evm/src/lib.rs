53bb6af301dbdf00cbb7a263c938cec28b8d01b0
Cameron Fairchild <cameron@opentensor.ai>
2025-01-06 11:19:51 -0500

    use unwrap or

--- frame/evm/src/lib.rs
+++ frame/evm/src/lib.rs
@@ -1050,8 +1050,8 @@ where
 			let account_id = T::AddressMapping::into_account_id(*who);
 
 			// Convert corrected fee into substrate balance
-			let corrected_fee_sub = T::BalanceConverter::into_substrate_balance(corrected_fee)
-				.ok_or(Error::<T>::FeeOverflow)?;
+			let corrected_fee_sub =
+				T::BalanceConverter::into_substrate_balance(corrected_fee).unwrap_or(U256::from(0));
 
 			// Calculate how much refund we should return
 			let refund_amount = paid
@@ -1089,8 +1089,8 @@ where
 				.unwrap_or_else(|_| C::NegativeImbalance::zero());
 
 			// Convert base fee into substrate balance
-			let base_fee_sub = T::BalanceConverter::into_substrate_balance(base_fee)
-				.ok_or(Error::<T>::FeeOverflow)?;
+			let base_fee_sub =
+				T::BalanceConverter::into_substrate_balance(base_fee).unwrap_or(U256::from(0));
 
 			let (base_fee, tip) = adjusted_paid.split(base_fee_sub.unique_saturated_into());
 			// Handle base fee. Can be either burned, rationed, etc ...
@@ -1158,8 +1158,8 @@ where
 			let account_id = T::AddressMapping::into_account_id(*who);
 
 			// Convert corrected fee into substrate balance
-			let corrected_fee_sub = T::BalanceConverter::into_substrate_balance(corrected_fee)
-				.ok_or(Error::<T>::FeeOverflow)?;
+			let corrected_fee_sub =
+				T::BalanceConverter::into_substrate_balance(corrected_fee).unwrap_or(U256::from(0));
 
 			// Calculate how much refund we should return
 			let refund_amount = paid
@@ -1176,8 +1176,8 @@ where
 				.unwrap_or_else(|_| Credit::<T::AccountId, F>::zero());
 
 			// Convert base fee into substrate balance
-			let base_fee_sub = T::BalanceConverter::into_substrate_balance(base_fee)
-				.ok_or(Error::<T>::FeeOverflow)?;
+			let base_fee_sub =
+				T::BalanceConverter::into_substrate_balance(base_fee).unwrap_or(U256::from(0));
 
 			let (base_fee, tip) = adjusted_paid.split(base_fee_sub.unique_saturated_into());
 			// Handle base fee. Can be either burned, rationed, etc ...
