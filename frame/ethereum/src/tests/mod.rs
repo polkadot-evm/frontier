e7671b1708a16a58742c7a9a8e6f8d9d200345c7
Manuel Mauro <manuel.mauro@protonmail.com>
2025-06-20 11:21:17 +0300

    test: :fire: temporarily remove EIP-7702 tests

--- frame/ethereum/src/tests/mod.rs
+++ frame/ethereum/src/tests/mod.rs
@@ -31,7 +31,6 @@ use fp_self_contained::CheckedExtrinsic;
 
 mod eip1559;
 mod eip2930;
-mod eip7702;
 mod legacy;
 
 // This ERC-20 contract mints the maximum amount of tokens to the contract creator.
