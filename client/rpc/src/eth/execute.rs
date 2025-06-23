3d11e21fe65f4199cc5a6714d0e1673bc037cd7c
Manuel Mauro <manuel.mauro@protonmail.com>
2025-06-23 12:27:48 +0300

    style: :art: fmt

--- client/rpc/src/eth/execute.rs
+++ client/rpc/src/eth/execute.rs
@@ -389,26 +389,25 @@ where
 					// Post-london + access list support
 					let access_list = access_list.unwrap_or_default();
 					#[allow(deprecated)]
-					let info = api
-						.create_before_version_6(
-							substrate_hash,
-							from.unwrap_or_default(),
-							data,
-							value.unwrap_or_default(),
-							gas_limit,
-							max_fee_per_gas,
-							max_priority_fee_per_gas,
-							nonce,
-							false,
-							Some(
-								access_list
-									.into_iter()
-									.map(|item| (item.address, item.storage_keys))
-									.collect(),
-							),
-						)
-						.map_err(|err| internal_err(format!("runtime error: {err}")))?
-						.map_err(|err| internal_err(format!("execution fatal: {err:?}")))?;
+					let info = api.create_before_version_6(
+						substrate_hash,
+						from.unwrap_or_default(),
+						data,
+						value.unwrap_or_default(),
+						gas_limit,
+						max_fee_per_gas,
+						max_priority_fee_per_gas,
+						nonce,
+						false,
+						Some(
+							access_list
+								.into_iter()
+								.map(|item| (item.address, item.storage_keys))
+								.collect(),
+						),
+					)
+					.map_err(|err| internal_err(format!("runtime error: {err}")))?
+					.map_err(|err| internal_err(format!("execution fatal: {err:?}")))?;
 
 					error_on_execution_failure(&info.exit_reason, &[])?;
 
