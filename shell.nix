5d4bb9ceb6c3be1025979fddd118dddaa73f32d6
Wei Tang <wei@pacna.org>
2025-07-03 06:41:21 +0200

    Update nix support

--- shell.nix
+++ shell.nix
@@ -2,10 +2,10 @@ let
   mozillaOverlay =
     import (builtins.fetchGit {
       url = "https://github.com/mozilla/nixpkgs-mozilla.git";
-      rev = "78e723925daf5c9e8d0a1837ec27059e61649cb6";
+      rev = "2292d4b35aa854e312ad2e95c4bb5c293656f21a";
     });
   nixpkgs = import <nixpkgs> { overlays = [ mozillaOverlay ]; };
-  rust-stable = with nixpkgs; ((rustChannelOf { date = "2024-10-15"; channel = "stable"; }).rust.override {
+  rust-stable = with nixpkgs; ((rustChannelOf { date = "2025-07-01"; channel = "nightly"; }).rust.override {
     extensions = [ "rust-src" ];
     targets = [ "wasm32-unknown-unknown" ];
   });
@@ -27,4 +27,6 @@ with nixpkgs; pkgs.mkShell {
   LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
   PROTOC = "${protobuf}/bin/protoc";
   ROCKSDB_LIB_DIR = "${rocksdb}/lib";
+  CFLAGS = "-Wno-error=int-conversion";
+  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
 }
