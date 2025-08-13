46d438a6145dc5472f293babddc9a0c3378ec9c0
Éloïs <c@elo.tf>
2025-08-13 19:38:05 +0200

    for some reason pallet calls macro generate useless conversion

--- frame/evm/src/lib.rs
+++ frame/evm/src/lib.rs
@@ -53,7 +53,7 @@
 // Ensure we're `no_std` when compiling for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
 #![warn(unused_crate_dependencies)]
-#![allow(clippy::too_many_arguments)]
+#![allow(clippy::too_many_arguments, clippy::useless_conversion)]
 
 extern crate alloc;
 
@@ -287,7 +287,6 @@ pub mod pallet {
 	}
 
 	#[pallet::call]
-	#[allow(clippy::useless_conversion)]
 	impl<T: Config> Pallet<T> {
 		/// Withdraw balance from EVM into currency/balances pallet.
 		#[pallet::call_index(0)]
