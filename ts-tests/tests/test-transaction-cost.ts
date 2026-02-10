5f8ae3a9632dbcbf286127950fa6a75814ae04e6
Éloïs <c@elo.tf>
2026-02-10 18:23:31 +0100

    fix ts-test test-transaction-cost

--- ts-tests/tests/test-transaction-cost.ts
+++ ts-tests/tests/test-transaction-cost.ts
@@ -5,11 +5,11 @@ import { describeWithFrontier, customRequest } from "./util";
 
 describeWithFrontier("Frontier RPC (Transaction cost)", (context) => {
 	step("should take transaction cost into account and not submit it to the pool", async function () {
-		// Simple transfer with gas limit 0 manually signed to prevent web3 from rejecting client-side.
+		// Protected (EIP-155) legacy tx with gas limit 0, signed offline.
 		const tx = await customRequest(context.web3, "eth_sendRawTransaction", [
-			"0xf86180843b9aca00809412cb274aad8251c875c0bf6872b67d9983e53fdd01801ca00e28ba2dd3c5a3fd467\
-			d4afd7aefb4a34b373314fff470bb9db743a84d674a0aa06e5994f2d07eafe1c37b4ce5471caecec29011f6f5b\
-			f0b1a552c55ea348df35f",
+			"0xf86180843b9aca00809412cb274aad8251c875c0bf6872b67d9983e53fdd0180" +
+				"77a05e311790fcf3eef1ed6d9ee6ffeea8c44a677b52168240b5713dacb0b85cf203" +
+				"a05e55ddeb10d2ad84fdf6901ba0047d5c311de899552592e43f1d11b4947527c6",
 		]);
 		let msg = "intrinsic gas too low";
 		expect(tx.error).to.include({
